6c6154781530c5a354b40e3c43908bff76698570
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-12-05 15:49:36 +0100

    Update 7-gettin_cozy.py

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -9,7 +9,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """
     This method concatenates two matrices
     """
-    if len(mat1) != len(mat2):
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat[1]):
         return None
     if axis == 0:
         return mat1 + mat2
