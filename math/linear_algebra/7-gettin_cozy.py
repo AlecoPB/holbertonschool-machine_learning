2f75de0e397caed720e6000047320519abc9faa9
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-12-05 15:53:23 +0100

    Update 7-gettin_cozy.py

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -11,7 +11,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """
     if axis == 0:
         return mat1 + mat2
-    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[1]):
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
     cat_mat = []
     for i in range(len(mat1)):
