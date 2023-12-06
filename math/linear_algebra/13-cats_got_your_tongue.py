8ba4ece7a15b80e230f42632ee9efb9f99d5e08c
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-12-06 15:05:38 +0100

    Create 13-cats_got_your_tongue.py

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module has only one method
+"""
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0):
+    """
+    This method concatenates two matrices along an axis
+    """
+    np.concatenate((mat1, mat2), axis)
