9f5d55bd3c2f87e91bdc2f018d335978e6310d21
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-12-06 11:32:30 +0100

    Update 10-ill_use_my_scale.py

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -5,8 +5,7 @@ This module serves almost no purpose
 
 
 def np_shape(matrix):
-  """
-  This method is literally just renaming np.shape
-  """
-  return matrix.shape
-  
+    """
+    This method is literally just renaming np.shape
+    """
+    return matrix.shape
