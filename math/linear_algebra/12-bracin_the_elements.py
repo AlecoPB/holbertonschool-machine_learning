8f6595b79e13fd9a784946366bed4ea7799a88bd
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-12-06 14:55:54 +0100

    Update 12-bracin_the_elements.py

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -8,8 +8,8 @@ def np_elementwise(mat1, mat2):
     """
     This method does basic operations
     """
-    add = np.add(mat1, mat2)
-    sub = np.subtract(mat1, mat2)
-    mul = np.multiply(mat1, mat2)
-    div = np.divide(mat1, mat2)
+    add = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
     return add, sub, mul, div
