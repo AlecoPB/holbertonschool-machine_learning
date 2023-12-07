608e6d651625d6b446b4b3d93c71e628c0ddb24b
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-12-07 10:20:24 +0100

    Create 0-line.py

---
+++ math/plotting/0-line.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module plots an exponential function
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+y = np.arange(0, 11) ** 3
+x = np.arange(0, 11)
+
+plt.plot(x, y, "r-")
+plt.show()
