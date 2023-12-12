aa6f2c855137658fabe9f4735332fa72e21e6008
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-12-12 11:51:53 +0100

    Update 9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -9,5 +9,5 @@ def summation_i_squared(n):
     This method does is the total sum of n squares
     """
     if not isinstance(n, int) or n <= 0:
-      return None
+        return None
     return n * (n + 1) * (2 * n + 1) // 6
