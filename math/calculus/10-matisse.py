6051b32e5e1edc814f1a29bcec9a9503eb7dc76d
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-12-12 12:12:08 +0100

    Update 10-matisse.py

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 def poly_derivative(poly):
-  if not isinstance(poly, list) or len(poly) == 0:
-    return None
-  dev_poly = [0]
-  if poly == [0]:
-      return dev_poly
-  for coeff in range(1, len(poly)):
-      if coeff == 1:
-          dev_poly[0] = poly[coeff] * coeff
-      else:
-          dev_poly.append(poly[coeff] * coeff)
-  return dev_poly
+    if not isinstance(poly, list) or len(poly) == 0:
+        return None
+    dev_poly = [0]
+    if poly == [0]:
+        return dev_poly
+    for coeff in range(1, len(poly)):
+        if coeff == 1:
+            dev_poly[0] = poly[coeff] * coeff
+        else:
+            dev_poly.append(poly[coeff] * coeff)
+    return dev_poly
