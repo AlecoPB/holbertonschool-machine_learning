b64932ff4a66a69628630f43f60f23a701c1768b
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-12-12 11:23:56 +0100

    Update 9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 def summation_i_squared(n):
   res = 0
-  for i in range(1, n+1):
-    res += i**2
-  return res
+  if n == 0:
+    return res
+  else:
+    res = n**2+summation_i_squared(n-1)
+    return res
