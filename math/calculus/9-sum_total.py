006156eef474a4af21bb4ce73b91d5668cd08114
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-12-12 11:51:20 +0100

    Update 9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
+"""
+This module has only one method
+"""
+
+
 def summation_i_squared(n):
-  if not isinstance(n, int) or n <= 0:
-    return None
-  return n * (n + 1) * (2 * n + 1) // 6
+    """
+    This method does is the total sum of n squares
+    """
+    if not isinstance(n, int) or n <= 0:
+      return None
+    return n * (n + 1) * (2 * n + 1) // 6
