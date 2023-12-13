16a44e12da170d11c5134725868a9fa03e28534f
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-12-13 10:31:51 +0100

    Update 101-the_whole_barn.py

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -9,19 +9,19 @@ def matrix_shape(matrix):
     Returns the shape of a matrix.
     """
     if not matrix:
-       return []
+        return []
     if isinstance(matrix[0], list):
-       return [len(matrix)] + matrix_shape(matrix[0])
+        return [len(matrix)] + matrix_shape(matrix[0])
     else:
-       return [len(matrix)]
+        return [len(matrix)]
 
 def add_matrices(mat1, mat2):
     """
     This method adds up two ndmatrices
     """
     if matrix_shape(mat1) != matrix_shape(mat2):
-       return None
+        return None
     if isinstance(mat1[0], list):
-       return [add_matrices(m1, m2) for m1, m2 in zip(mat1, mat2)]
+        return [add_matrices(m1, m2) for m1, m2 in zip(mat1, mat2)]
     else:
-       return [m1 + m2 for m1, m2 in zip(mat1, mat2)]
+        return [m1 + m2 for m1, m2 in zip(mat1, mat2)]
