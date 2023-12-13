88e78a8fbdc07b49e064c2d68de7ebace06ae3b0
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-12-13 10:18:20 +0100

    Update 101-the_whole_barn.py

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 """
-This module has only one method
+This module has only one method, unless they make me use a second method
 """
-import numpy as np
 
+
+def matrix_shape(matrix):
+  """
+  Returns the shape of a matrix.
+  """
+  if not matrix:
+    return []
+  if isinstance(matrix[0], list):
+    return [len(matrix)] + matrix_shape(matrix[0])
+  else:
+    return [len(matrix)]
 
 def add_matrices(mat1, mat2):
     """
     This method adds up two ndmatrices
     """
-    if np.shape(mat1) != np.shape(mat2):
+    if matrix_shape(mat1) != matrix_shape(mat2):
        return None
     if isinstance(mat1[0], list):
        return [add_matrices(m1, m2) for m1, m2 in zip(mat1, mat2)]
