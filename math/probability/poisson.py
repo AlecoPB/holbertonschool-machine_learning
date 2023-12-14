4da190e5e3d4a428efe4ad02dabb0409e77b45c8
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-12-14 13:29:27 +0100

    Update poisson.py

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -58,6 +58,5 @@ class Poisson:
             k = int(k)
         cdf_value = 0
         for i in range(k+1):
-
-            cdf_value +=\
-                        (e**(-1*self.lambtha)*self.lambtha**(i))/Poisson.fact(i)
+            cdf_value += (e**(-1*self.lambtha)*\
+                          self.lambtha**(i))/Poisson.fact(i)
