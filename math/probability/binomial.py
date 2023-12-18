43fa3697b54df62db5744a284ca6070c0696e1c9
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-12-18 12:06:17 +0100

    Update binomial.py

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -22,13 +22,11 @@ class Binomial:
             else:
                 mean = sum(data) / len(data)
                 squares = [(x - mean) ** 2 for x in data]
-                var = sum(squares) / len(squares)
-                self.p = 1 - (var / mean)
-                if round(mean / self.p) <= 0:
-                    raise ValueError("n must be a non-negative value")
-                else:
-                    self.n = round(mean / self.p)
-                if not 0 < mean / self.n < 1:
-                    raise ValueError("p must be greater than or equal to 0 and less than or equal to 1")
-                else:
-                    self.p = mean / self.n
+                var = sum(squared_diffs) / len(squared_diffs)
+                self.p = 1 - (variance / mean)
+                self.n = mean / self.p
+                self.p = self.p / self.n
+        if self.n <= 0:
+            raise ValueError("n must be a non-negative value")
+        elif not 0 <= self.p <= 1:
+            raise ValueError("p must be greater than or equal to 0 and less than or equal to 1")
