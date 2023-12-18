e6acbfa5d751d1db2495fafdaac8e84ea701d112
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-12-18 10:47:01 +0100

    Create binomial.py

---
+++ math/probability/binomial.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+class Binomial:
+    """
+    This module defines a Binomial distribution
+    """
+    pi = 3.1415926536
+    e = 2.7182818285
+    
+    def __init__(self, data=None, n=1, p=0.5):
+        if data is None:
+            self.n = int(n)
+            self.p = float(p)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                self.p = sum(data) / len(data)
+                self.n = max(data)
+                self.p = self.p / self.n
