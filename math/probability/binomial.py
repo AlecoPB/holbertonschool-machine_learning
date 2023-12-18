84a88ed67d00a738a98542a2dbfc2e6195f4415e
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-12-18 11:50:22 +0100

    Update binomial.py

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -20,14 +20,15 @@ class Binomial:
             elif len(data) < 2:
                 raise ValueError("data must contain multiple values")
             else:
-                mean = sum(data) / len(data)
-                squares = [(x - mean) ** 2 for x in data]
-                var = sum(squares) / len(squares)
-
-                self.p = 1 - (var / mean)
-                self.n = round(mean / self.p)
-                self.p = mean / self.n
                 if self.n < 0:
                     raise ValueError("n must be a positive value")
                 elif not 0 < self.p < 1:
                     raise ValueError("p must be greater than 0 and less than 1")
+                else:
+                    mean = sum(data) / len(data)
+                    squares = [(x - mean) ** 2 for x in data]
+                    var = sum(squares) / len(squares)
+
+                    self.p = 1 - (var / mean)
+                    self.n = round(mean / self.p)
+                    self.p = mean / self.n
