83df30b0d24897d13a331137d6cb570a8dbddcb4
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2024-01-03 14:37:22 +0100

    Update 0-neuron.py

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -12,10 +12,10 @@ class Neuron:
     and checks for errors
     """
     def __init__(self, nx):
-        if not isinstance(int, nx):
+        if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         elif nx < 1:
             raise ValueError("nx must be a positive integer")
-        self.W = np.random.normal()
+        self.W = np.random.normal(loc=0.0, scale=1.0, size=None)
         self.b = 0
         self.A = 0
