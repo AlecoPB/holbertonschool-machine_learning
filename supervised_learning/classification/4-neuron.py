d49ba7927a81cc02b95d228516368892f1e15b6a
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2024-01-04 17:33:56 +0100

    Create 4-neuron.py

---
+++ supervised_learning/classification/4-neuron.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""
+This module defines the Neuron class
+"""
+import numpy as np
+
+
+class Neuron:
+    """
+    This is the Neuron module
+    The __init__ function initializes attributes
+    and checks for errors
+    """
+    def __init__(self, nx):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.normal(loc=0.0, scale=1.0, size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
+
+    def forward_prop(self, X):
+        """
+        Args:
+            X is an array containing input sets
+
+        Returns:
+            The private attribute A as an array
+        """
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """
+
+        Args:
+            Y (np.array): Correct labels for the data
+            A (np.array): Activated outputs
+
+        Returns:
+            Cost
+        """
+        m = np.shape(Y)[1]
+        sum = np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return (-1 / m) * sum
+
+    def evaluate(self, X, Y):
+        act_X = self.forward_prop(X)
+        cost = self.cost(Y, act_X)
+        act_X = (act_X > 0.5).astype(int)
+        return act_X, cost
