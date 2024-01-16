3d148d28f7c5cd3128494a4ab417a4ca9a21bb45
AlecoPB <ajoaquinpg@gmail.com>
2024-01-16 11:45:35 +0100

    Create 25-one_hot_decode.py

---
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+_summary_
+Method to decode a one_hot matrix
+"""
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """_summary_
+
+    Args:
+        one_hot (np.ndarray): One_hot encoded matrix
+        with shape (classes, m)
+    """
+    if not isinstance(one_hot, np.ndarray) or len(one_hot) == 0:
+        return None
+    try:
+        return np.argmax(one_hot, axis=2)
+    except Exception as e:
+        return None
