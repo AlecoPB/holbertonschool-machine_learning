2c8267f24800551e9a5e6c05a862c4058b7daefb
AlecoPB <ajoaquinpg@gmail.com>
2024-01-16 11:56:19 +0100

    Update 25-one_hot_decode.py

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -13,7 +13,9 @@ def one_hot_decode(one_hot):
         one_hot (np.ndarray): One_hot encoded matrix
         with shape (classes, m)
     """
+    if not isinstance(one_hot, np.ndarray) or len(one_hot) == 0:
+        return None
     try:
-        return np.argmax(one_hot, axis=0)
+        return np.argmax(one_hot.T, axis=1)
     except Exception as e:
         return None
