17fcd19831421a8db1b1f8b7338088c2ea955a0d
AlecoPB <ajoaquinpg@gmail.com>
2024-01-19 15:53:26 +0100

    Update 0-create_placeholders.py

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
+"""_summary_
+
+Returns:
+    _type_: _description_
+"""
 import tensorflow.compat.v1 as tf
 
 
 def create_placeholders(nx, classes):
+    """
+    Args:
+        nx (int): number of feature columns in our data
+        classes (int): number of classes in our classifier
+
+    Returns:
+        _type_: placeholders 
+    """
     x = tf.placeholder(dtype=tf.float32, shape=[None, nx], name='x')
     y = tf.placeholder(dtype=tf.float32, shape=[None, classes], name='y')
     return x, y
