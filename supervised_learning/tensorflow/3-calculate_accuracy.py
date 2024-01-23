68002cbd42832685584483a9cfa100d9f120325a
AlecoPB <ajoaquinpg@gmail.com>
2024-01-23 15:35:33 +0100

    Update 3-calculate_accuracy.py

--- supervised_learning/tensorflow/3-calculate_accuracy.py
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -8,7 +8,15 @@ import tensorflow.compat.v1 as tf
 def calculate_accuracy(y, y_pred):
     """
     Args:
-        y (_type_): _description_
-        y_pred (_type_): _description_
+        y (_type_): Placeholder for the labels of the input data
+        y_pred (_type_): Tensor containing the network’s predictions
+    Returns:
+        A tensor containing the decimal accuracy of the prediction
     """
-    return y/y_pred
+    # Calculate correct predictions
+    correct_predictions = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y, 1))
+
+    # Calculate accuracy
+    accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
+
+    return accuracy
