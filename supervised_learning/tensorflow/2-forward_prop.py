d4444ae66541a50685685726b0e537229f876838
AlecoPB <ajoaquinpg@gmail.com>
2024-01-23 11:59:51 +0100

    Update 2-forward_prop.py

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import tensorflow.compat.v1 as tf
-create_layer = __import__('1-create_layer').create_layer
 """
 This is some documentation
 """
+create_layer = __import__('1-create_layer').create_layer
 
 
 def forward_prop(x, layer_sizes=[], activations=[]):
