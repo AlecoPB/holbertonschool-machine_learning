7bb7aab1d019e05f86bd586bd442fd915c653e1f
AlecoPB <ajoaquinpg@gmail.com>
2024-01-23 15:17:14 +0100

    Create 3-calculate_accuracy.py

---
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This is some documentation
+"""
+import tensorflow as tf
+
+
+def calculate_accuracy(y, y_pred):
+    """
+    Args:
+        y (_type_): _description_
+        y_pred (_type_): _description_
+    """
+    return y/y_pred
