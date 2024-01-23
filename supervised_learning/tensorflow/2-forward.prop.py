caf9b06f92f943922b34ed2030536a62baa2fc9b
AlecoPB <ajoaquinpg@gmail.com>
2024-01-23 11:34:23 +0100

    Create 2-forward.prop.py

---
+++ supervised_learning/tensorflow/2-forward.prop.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+create_layer = __import__('1-create_layer').create_layer
+"""
+This is some documentation
+"""
+
+
+def forward_prop(x, layer_sizes=[], activations=[]):
+    """
+    Args:
+        x (_type_): _description_
+        layer_sizes (list, optional): _description_. Defaults to [].
+        activations (list, optional): _description_. Defaults to [].
+    """
+    layers = []
+    for i in range(len(layers)):
+        if i == 0:
+            layers.append(create_layer(x, layer_sizes[i], activations[i]))
+        else:
+            layers.append(create_layer(layers[i - 1], layer_sizes[i], activations[i]))
+    return layers
