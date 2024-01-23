6e21cd8c0f64462a33cce0625233eeb43fcdf4b7
AlecoPB <ajoaquinpg@gmail.com>
2024-01-23 11:09:04 +0100

    Create 1-create_layer.py

---
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+"""
+This is some documentation
+"""
+
+
+def create_layer(prev, n, activation):
+    """_summary_
+
+    Args:
+        prev (_type_): _description_
+        n (_type_): _description_
+        activation (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    layer = tf.keras.layers.Dense(n, activation=activation, kernel_initializer=initializer, name='layer')(prev)
+    return layer
