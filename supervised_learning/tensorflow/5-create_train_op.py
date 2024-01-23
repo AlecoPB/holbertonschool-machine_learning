185e5ef3f26edfdad5ac83fb0ba0410379f19687
AlecoPB <ajoaquinpg@gmail.com>
2024-01-23 16:45:56 +0100

    Create 5-create_train_op.py

---
+++ supervised_learning/tensorflow/5-create_train_op.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This is some documentation
+"""
+import tensorflow as tf
+
+
+def create_train_op(loss, alpha):
+    """
+    Args:
+        y (_type_): Placeholder for the labels of the input data
+        y_pred (_type_): Tensor containing the network’s predictions
+    Returns:
+        A tensor containing the loss of the prediction
+    """
+    optimizer = tf.train.GradientDescentOptimizer(alpha)
+    train_op = optimizer.minimize(loss)
+    return train_op
