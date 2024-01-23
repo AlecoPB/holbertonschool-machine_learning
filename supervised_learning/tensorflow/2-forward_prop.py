137350b1bb145b1f2c139c731bbd12ad050169c6
AlecoPB <ajoaquinpg@gmail.com>
2024-01-23 12:03:51 +0100

    Update 2-forward_prop.py

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-import tensorflow.compat.v1 as tf
 """
 This is some documentation
 """
+import tensorflow.compat.v1 as tf
 create_layer = __import__('1-create_layer').create_layer
 
 
