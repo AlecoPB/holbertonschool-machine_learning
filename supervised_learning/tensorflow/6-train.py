da813f115c0f9085fc779c26fd70f11b69037562
AlecoPB <ajoaquinpg@gmail.com>
2024-01-24 14:21:44 +0100

    Update 6-train.py

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -39,7 +39,7 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
     tf.add_to_collection('y_pred', y_pred)
     tf.add_to_collection('accuracy', accuracy)
     tf.add_to_collection('loss', loss)
-    tf.add_to_collection('train_op', train_op)
+    # tf.add_to_collection('train_op', train_op)
 
     init = tf.global_variables_initializer()
     with tf.Session() as sess:
