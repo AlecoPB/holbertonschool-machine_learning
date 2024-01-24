fcabebc7e4813ef8ef53c489b8713f041441164d
AlecoPB <ajoaquinpg@gmail.com>
2024-01-24 12:22:51 +0100

    Update 6-train.py

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -2,37 +2,31 @@
 """
 This is some documentation
 """
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
+
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
 create_placeholders = __import__('0-create_placeholders').create_placeholders
 create_train_op = __import__('5-create_train_op').create_train_op
 forward_prop = __import__('2-forward_prop').forward_prop
 
-
-def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
-          activations, alpha, iterations, save_path="/tmp/model.ckpt"):
-    """_summary_
-
-    Args:
-        X_train (_type_): _description_
-        Y_train (_type_): _description_
-        X_valid (_type_): _description_
-        Y_valid (_type_): _description_
-        layer_sizes (_type_): _description_
-        activations (_type_): _description_
-        alpha (_type_): _description_
-        iterations (_type_): _description_
-        save_path (str, optional): _description_.
-
-    Returns:
-        _type_: _description_
-    """
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
+    # Create placeholders
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
-    y_pred = forward_prop(X_train, layer_sizes, activations)
-    accuracy = calculate_accuracy(y_pred, Y_train)
-    loss = calculate_loss(y_pred, Y_train)
+
+    # Forward propagation
+    y_pred = forward_prop(x, layer_sizes, activations)
+
+    # Calculate accuracy
+    accuracy = calculate_accuracy(y_pred, y)
+
+    # Calculate loss
+    loss = calculate_loss(y_pred, y)
+
+    # Create training operation
     train_op = create_train_op(loss, alpha)
+
+    # Add to collection
     tf.add_to_collection('x', x)
     tf.add_to_collection('y', y)
     tf.add_to_collection('y_pred', y_pred)
@@ -40,31 +34,97 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
     tf.add_to_collection('loss', loss)
     tf.add_to_collection('train_op', train_op)
 
+    # Initialize all variables
     init = tf.global_variables_initializer()
 
     # Start a session
     with tf.Session() as sess:
         sess.run(init)
         for i in range(iterations):
+            # Run the training operation and calculate the training loss and accuracy
+            _, train_loss, train_accuracy = sess.run([train_op, loss, accuracy], feed_dict={x: X_train, y: Y_train})
 
-            _, train_loss, train_accuracy =\
-                sess.run([train_op, loss, accuracy], feed_dict={x: X_train,
-                                                                y: Y_train})
-            print("IN")
+            # Every 100 iterations, validate the model
             if i % 100 == 0 or i == iterations:
                 # Calculate the validation loss and accuracy
+                valid_loss, valid_accuracy = sess.run([loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
 
-                valid_loss, valid_accuracy =\
-                    sess.run([loss, accuracy], feed_dict={x: X_valid,
-                                                          y: Y_valid})
                 print("After {} iterations:".format(i))
                 print("\tTraining Cost: {}".format(train_loss))
                 print("\tTraining Accuracy: {}".format(train_accuracy))
                 print("\tValidation Cost: {}".format(valid_loss))
                 print("\tValidation Accuracy: {}".format(valid_accuracy))
-            print("OUT")
+
         # Save the model
         saver = tf.train.Saver()
         save_path = saver.save(sess, save_path)
 
     return save_path
+
+# import tensorflow.compat.v1 as tf
+# calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+# calculate_loss = __import__('4-calculate_loss').calculate_loss
+# create_placeholders = __import__('0-create_placeholders').create_placeholders
+# create_train_op = __import__('5-create_train_op').create_train_op
+# forward_prop = __import__('2-forward_prop').forward_prop
+
+
+# def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
+#           activations, alpha, iterations, save_path="/tmp/model.ckpt"):
+#     """_summary_
+
+#     Args:
+#         X_train (_type_): _description_
+#         Y_train (_type_): _description_
+#         X_valid (_type_): _description_
+#         Y_valid (_type_): _description_
+#         layer_sizes (_type_): _description_
+#         activations (_type_): _description_
+#         alpha (_type_): _description_
+#         iterations (_type_): _description_
+#         save_path (str, optional): _description_.
+
+#     Returns:
+#         _type_: _description_
+#     """
+#     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
+#     y_pred = forward_prop(X_train, layer_sizes, activations)
+#     accuracy = calculate_accuracy(y_pred, Y_train)
+#     loss = calculate_loss(y_pred, Y_train)
+#     train_op = create_train_op(loss, alpha)
+#     tf.add_to_collection('x', x)
+#     tf.add_to_collection('y', y)
+#     tf.add_to_collection('y_pred', y_pred)
+#     tf.add_to_collection('accuracy', accuracy)
+#     tf.add_to_collection('loss', loss)
+#     tf.add_to_collection('train_op', train_op)
+
+#     init = tf.global_variables_initializer()
+
+#     # Start a session
+#     with tf.Session() as sess:
+#         sess.run(init)
+#         for i in range(iterations):
+
+#             _, train_loss, train_accuracy =\
+#                 sess.run([train_op, loss, accuracy], feed_dict={x: X_train,
+#                                                                 y: Y_train})
+#             print("IN")
+#             if i % 100 == 0 or i == iterations:
+#                 # Calculate the validation loss and accuracy
+
+#                 valid_loss, valid_accuracy =\
+#                     sess.run([loss, accuracy], feed_dict={x: X_valid,
+#                                                           y: Y_valid})
+#                 print("After {} iterations:".format(i))
+#                 print("\tTraining Cost: {}".format(train_loss))
+#                 print("\tTraining Accuracy: {}".format(train_accuracy))
+#                 print("\tValidation Cost: {}".format(valid_loss))
+#                 print("\tValidation Accuracy: {}".format(valid_accuracy))
+#             print("OUT")
+#         # Save the model
+#         saver = tf.train.Saver()
+#         save_path = saver.save(sess, save_path)
+
+#     return save_path
+# """
