2e52012f73488645a7d1e093d06eba258ce9fc51
AlecoPB <ajoaquinpg@gmail.com>
2024-02-13 14:37:39 +0100

    Update 6-train.py

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -38,20 +38,20 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
     tf.add_to_collection('y_pred', y_pred)
     tf.add_to_collection('accuracy', accuracy)
     tf.add_to_collection('loss', loss)
-    sess = tf.Session()
-    sess.run(tf.global_variables_initializer())
-    for i in range(iterations):
-        sess.run(train_op, feed_dict={x: X_train, y: Y_train})
-        if i % 100 == 0 or i == iterations:
-            train_accuracy, train_loss = sess.run([accuracy, loss], feed_dict={x: X_train, y: Y_train})
-            valid_accuracy, valid_loss = sess.run([accuracy, loss], feed_dict={x: X_valid, y: Y_valid})
-            print("After {} iterations:".format(i))
-            print("\tTraining Cost: {}".format(train_loss))
-            print("\tTraining Accuracy: {}".format(train_accuracy))
-            print("\tValidation Cost: {}".format(valid_loss))
-            print("\tValidation Accuracy: {}".format(valid_accuracy))
-    saver = tf.train.Saver()
-    saver.save(sess, save_path)
-    sess.close()
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for i in range(iterations):
+            sess.run(train_op, feed_dict={x: X_train, y: Y_train})
+            if i % 100 == 0 or i == iterations:
+                train_accuracy, train_loss = sess.run([accuracy, loss], feed_dict={x: X_train, y: Y_train})
+                valid_accuracy, valid_loss = sess.run([accuracy, loss], feed_dict={x: X_valid, y: Y_valid})
+                print("After {} iterations:".format(i))
+                print("\tTraining Cost: {}".format(train_loss))
+                print("\tTraining Accuracy: {}".format(train_accuracy))
+                print("\tValidation Cost: {}".format(valid_loss))
+                print("\tValidation Accuracy: {}".format(valid_accuracy))
+
+        saver = tf.train.Saver()
+        save_path = saver.save(sess, save_path)
 
     return save_path
