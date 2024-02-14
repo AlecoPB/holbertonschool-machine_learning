987dd1ec603a3b06399fe9db6cb395653ecf96a3
AlecoPB <ajoaquinpg@gmail.com>
2024-02-14 15:24:14 +0100

    Update 6-train.py

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -1,40 +1,18 @@
-#!/usr/bin/env python3
-"""
-This is some documentation
-"""
-import tensorflow.compat.v1 as tf
-calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
-calculate_loss = __import__('4-calculate_loss').calculate_loss
-create_placeholders = __import__('0-create_placeholders').create_placeholders
-create_train_op = __import__('5-create_train_op').create_train_op
-forward_prop = __import__('2-forward_prop').forward_prop
+import tensorflow as tf
 
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
+    calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+    calculate_loss = __import__('4-calculate_loss').calculate_loss
+    create_placeholders = __import__('0-create_placeholders').create_placeholders
+    create_train_op = __import__('5-create_train_op').create_train_op
+    forward_prop = __import__('2-forward_prop').forward_prop
 
-def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
-          activations, alpha, iterations, save_path="/tmp/model.ckpt"):
-    """_summary_
-
-    Args:
-        X_train (_type_): _description_
-        Y_train (_type_): _description_
-        X_valid (_type_): _description_
-        Y_valid (_type_): _description_
-        layer_sizes (_type_): _description_
-        activations (_type_): _description_
-        alpha (_type_): _description_
-        iterations (_type_): _description_
-        save_path (str, optional): _description_.
-
-    Returns:
-        _type_: _description_
-    """
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
     y_pred = forward_prop(x, layer_sizes, activations)
-    print("print")
     loss = calculate_loss(y, y_pred)
     accuracy = calculate_accuracy(y, y_pred)
     train_op = create_train_op(loss, alpha)
-    
+
     init = tf.global_variables_initializer()
     saver = tf.train.Saver()
 
