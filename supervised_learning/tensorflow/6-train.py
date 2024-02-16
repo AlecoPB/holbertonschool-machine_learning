52cb56a7863d33210d1f5e88cab994e6ef56dc91
AlecoPB <ajoaquinpg@gmail.com>
2024-02-16 09:43:30 +0100

    Update 6-train.py

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python3
 import tensorflow.compat.v1 as tf
+
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
 create_placeholders = __import__('0-create_placeholders').create_placeholders
@@ -7,17 +7,15 @@ create_train_op = __import__('5-create_train_op').create_train_op
 forward_prop = __import__('2-forward_prop').forward_prop
 
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
-    
-    print("print")
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
     y_pred = forward_prop(x, layer_sizes, activations)
     loss = calculate_loss(y, y_pred)
     accuracy = calculate_accuracy(y, y_pred)
     train_op = create_train_op(loss, alpha)
-
+    
     init = tf.global_variables_initializer()
     saver = tf.train.Saver()
-
+    
     with tf.Session() as sess:
         sess.run(init)
         for i in range(iterations):
@@ -29,6 +27,5 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, i
                 print("\tTraining Accuracy: {}".format(t_acc))
                 print("\tValidation Cost: {}".format(v_cost))
                 print("\tValidation Accuracy: {}".format(v_acc))
-
-        save_path = saver.save(sess, save_path)
-    return save_path
+                
+        return saver.save(sess, save_path)
