32c2712d3a93b8923de6c0ad2a68c26efbe46ef3
AlecoPB <ajoaquinpg@gmail.com>
2024-02-27 16:50:39 +0100

    Create 0-norm_constants.py

---
+++ supervised_learning/optimization/0-norm_constants.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import numpy as np
+"""
+This is some documentation
+"""
+
+
+def normalization_constants(X):
+    """
+    Calculate the normalization constants of a matrix.
+
+    Parameters:
+    X (numpy.ndarray): The input matrix of shape (m, nx) to normalize.
+                       m is the number of data points.
+                       nx is the number of features.
+
+    Returns:
+    mean (numpy.ndarray): The mean of each feature.
+    std_dev (numpy.ndarray): The standard deviation of each feature.
+    """
+    # Calculate the mean of each feature
+    mean = np.mean(X, axis=0)
+
+    # Calculate the standard deviation of each feature
+    std_dev = np.std(X, axis=0)
+
+    return mean, std_dev
