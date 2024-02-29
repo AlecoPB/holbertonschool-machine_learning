2e6b3797585aa1e8a618440c072e494818dc9174
AlecoPB <ajoaquinpg@gmail.com>
2024-02-29 16:43:05 +0100

    Update 3-mini_batch.py

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -7,7 +7,7 @@ import tensorflow.compat.v1 as tf
 
 def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32, epochs=5, load_path="/tmp/model.ckpt", save_path="/tmp/model.ckpt"):
     shuffle_data = __import__('2-shuffle_data').shuffle_data
-
+    print("aaaaaaaaa")
     with tf.Session() as sess:
         # Import meta graph and restore session
         saver = tf.train.import_meta_graph(load_path + '.meta')
