bd52a1f3d0c3b939d6e72baeb0d8bee106d022a4
AlecoPB <ajoaquinpg@gmail.com>
2024-03-02 17:26:09 +0100

    Update 3-mini_batch.py

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -20,7 +20,7 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32, epochs=5
             X_shuffled, Y_shuffled = shuffle_data(X_train, Y_train)
             train_cost, train_accuracy = sess.run([loss, accuracy], feed_dict={x: X_train, y: Y_train})
             valid_cost, valid_accuracy = sess.run([loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
-            print(f"After {c_epoch} epochs:")#\tTraining Cost: {train_cost}\n\tTraining Accuracy: {train_accuracy}\n\tValidation Cost: {valid_cost}\n\tValidation Accuracy: {valid_accuracy}")
+            print("After {} epochs:\tTraining Cost: {}\tTraining Accuracy: {}\tValidation Cost: {}\tValidation Accuracy: {}".format(c_epoch, train_cost, train_accuracy, valid_cost, valid_accuracy))
             
             m = X_shuffled.shape[0]
             for i in range(0, m, batch_size):
