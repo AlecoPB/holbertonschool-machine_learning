75773fe026834f83ed2061f64639833c5ff57f76
AlecoPB <ajoaquinpg@gmail.com>
2024-03-05 16:39:00 +0100

    Update 3-mini_batch.py

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -7,7 +7,7 @@ shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
 def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
-                     epochs=5, load_path="/tmp/model.ckpt", 
+                     epochs=5, load_path="/tmp/model.ckpt",
                      save_path="/tmp/model.ckpt"):
     """_summary_
 
@@ -17,11 +17,11 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
         X_valid (_type_): _description_
         Y_valid (_type_): _description_
         batch_size (int, optional): _description_. Defaults to 32.
-        epochs (int, optional): _description_. 
+        epochs (int, optional): _description_.
         Defaults to 5.
-        load_path (str, optional) 
+        load_path (str, optional)
         Defaults to "/tmp/model.ckpt".
-        save_path (str, optional) 
+        save_path (str, optional)
         Defaults to "/tmp/model.ckpt".
 
     Returns:
@@ -30,17 +30,17 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
     with tf.Session() as sess:
         saver = tf.train.import_meta_graph(load_path + '.meta')
         saver.restore(sess, load_path)
-        
+
         x = tf.get_collection('x')[0]
         y = tf.get_collection('y')[0]
         accuracy = tf.get_collection('accuracy')[0]
         loss = tf.get_collection('loss')[0]
         train_op = tf.get_collection('train_op')[0]
-        
+
         m = X_train.shape[0]
         for c_epoch in range(epochs):
             X_shuffled, Y_shuffled = shuffle_data(X_train, Y_train)
-            
+
             train_cost, train_accuracy = sess.run([loss, accuracy], feed_dict={x: X_train, y: Y_train})
             valid_cost, valid_accuracy = sess.run([loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
             print(f"After {c_epoch} epochs:\n\tTraining Cost: {train_cost}\n\tTraining Accuracy: {train_accuracy}\n\tValidation Cost: {valid_cost}\n\tValidation Accuracy: {valid_accuracy}")
