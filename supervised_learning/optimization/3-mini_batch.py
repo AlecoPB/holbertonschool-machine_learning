275c0d0f505eca66af8a265a101c9d76767d7c6b
AlecoPB <ajoaquinpg@gmail.com>
2024-03-05 16:26:53 +0100

    Update 3-mini_batch.py

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -38,17 +38,15 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32, epochs=5
             train_cost, train_accuracy = sess.run([loss, accuracy], feed_dict={x: X_train, y: Y_train})
             valid_cost, valid_accuracy = sess.run([loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
             print(f"After {c_epoch} epochs:\n\tTraining Cost: 
-                  {train_cost}\n\tTraining Accuracy: {train_accuracy}\n\tValidation Cost: 
-                  {valid_cost}\n\tValidation Accuracy: {valid_accuracy}")
+                  {train_cost}\n\tTraining Accuracy: {train_accuracy}\n\tValidation Cost: {valid_cost}\n\tValidation Accuracy: {valid_accuracy}")
             for i in range(0, m, batch_size):
                 X_batch = X_shuffled[i : i + batch_size]
                 Y_batch = Y_shuffled[i : i + batch_size]
                 sess.run(train_op, feed_dict = {x : X_batch, y : Y_batch})
                 if ((i // batch_size) + 1) % 100 == 0:
                     step_cost, step_accuracy = sess.run([loss, accuracy], feed_dict={x: X_batch, y: Y_batch})
-                    if i != 0:
-                        print(f"\tStep {(i // batch_size) + 1}:\n\t\tCost: 
-                              {step_cost}\n\t\tAccuracy: {step_accuracy}")
+                    if i != 0: 
+                        print(f"\tStep {(i // batch_size) + 1}:\n\t\tCost: {step_cost}\n\t\tAccuracy: {step_accuracy}")
         train_cost, train_accuracy = sess.run([loss, accuracy], feed_dict={x: X_train, y: Y_train})
         valid_cost, valid_accuracy = sess.run([loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
         print(f"After {c_epoch + 1} epochs:\n\tTraining Cost: {train_cost}\n\tTraining Accuracy: {train_accuracy}\n\tValidation Cost: {valid_cost}\n\tValidation Accuracy: {valid_accuracy}")
