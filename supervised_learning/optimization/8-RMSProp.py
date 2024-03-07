af5c513b5c506dbe8c1503fa5d8f120587cddecf
AlecoPB <ajoaquinpg@gmail.com>
2024-03-07 15:25:55 +0100

    Create 8-RMSProp.py

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""_summary_
+
+Returns:
+    _type_: _description_
+"""
+import tensorflow.compat.v1 as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """
+    Creates the RMSProp optimization operation for a neural network.
+
+    Args:
+        loss (tf.Tensor): Loss of the network.
+        alpha (float): Learning rate.
+        beta2 (float): RMSProp weight.
+        epsilon (float): Small number to avoid division by zero.
+
+    Returns:
+        tf.Operation: The RMSProp optimization operation.
+    """
+    optimizer = tf.keras.optimizers.RMSprop(learning_rate=alpha, decay=beta2)
+    train_op = optimizer.minimize(loss)
+
+    return train_op
