681c58bbef656895673e6e325fbdc9b603e93984
AlecoPB <ajoaquinpg@gmail.com>
2024-03-07 16:24:58 +0100

    Create 10-Adam.py

---
+++ supervised_learning/optimization/10-Adam.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""_summary_
+This is some documentation
+"""
+import tensorflow.compat.v1 as tf
+
+
+def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+    """_summary_
+
+    Args:
+        loss (_type_): _description_
+        alpha (_type_): _description_
+        beta1 (_type_): _description_
+        beta2 (_type_): _description_
+        epsilon (_type_): _description_
+    """
+    optimizer = tf.keras.optimizers.Adam(learning_rate=alpha, beta_1=beta1, beta_2=beta2, epsilon=epsilon)
+    train_op = optimizer.minimize(loss)
+    return train_op
