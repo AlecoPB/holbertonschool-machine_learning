632b909e7d3fe567ed5f78d49b0daa79bcafe271
AlecoPB <ajoaquinpg@gmail.com>
2024-03-07 16:29:28 +0100

    Update 10-Adam.py

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -15,6 +15,6 @@ def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
         beta2 (_type_): _description_
         epsilon (_type_): _description_
     """
-    optimizer = tf.keras.optimizers.Adam(learning_rate=alpha, beta_1=beta1, beta_2=beta2, epsilon=epsilon)
+    optimizer = tf.train.AdamOptimizer(learning_rate=alpha, beta_1=beta1, beta_2=beta2, epsilon=epsilon)
     train_op = optimizer.minimize(loss)
     return train_op
