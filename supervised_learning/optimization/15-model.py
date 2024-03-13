66c6271d3daccadc1ed4dce2c476f3e84eb344cc
AlecoPB <ajoaquinpg@gmail.com>
2024-03-13 11:40:03 +0100

    Update 15-model.py

--- supervised_learning/optimization/15-model.py
+++ supervised_learning/optimization/15-model.py
@@ -96,20 +96,20 @@ def model(Data_train, Data_valid, layers, activations, alpha=0.001, beta1=0.9,
                     X_batch = X_train_shuffled[j: j + batch_size]
                     Y_batch = Y_train_shuffled[j: j + batch_size]
                     sess.run(train_op, feed_dict={x: X_batch, y: Y_batch})
-                    if ((j // batch_size) + 1) % 100 == 0:
-                        step_cost, step_accuracy =\
-                            sess.run([loss, accuracy],
-                                    feed_dict={x: X_batch, y: Y_batch})
-                        if j != 0:
-                            print(f"\tStep {(j // batch_size) + 1}:"
+                    # if ((j // batch_size) + 1) % 100 == 0:
+                    #     step_cost, step_accuracy =\
+                    #         sess.run([loss, accuracy],
+                    #                 feed_dict={x: X_batch, y: Y_batch})
+                    #     if j != 0:
+                    #         print(f"\tStep {(j // batch_size) + 1}:"
+                    #             f"\n\t\tCost: {step_cost}"
+                    #             f"\n\t\tAccuracy: {step_accuracy}")
+                    if j % 1000 == 0:
+                        step_cost, step_accuracy = sess.run([loss, accuracy], feed_dict={x: X_batch, y: Y_batch})
+                        # if i != 0:
+                        print(f"\tStep {j}:"
                                 f"\n\t\tCost: {step_cost}"
                                 f"\n\t\tAccuracy: {step_accuracy}")
-                # if j % 1000 == 0:
-                #     step_cost, step_accuracy = sess.run([loss, accuracy], feed_dict={x: X_batch, y: Y_batch})
-                #     # if i != 0:
-                #     print(f"\tStep {j}:"
-                #             f"\n\t\tCost: {step_cost}"
-                #             f"\n\t\tAccuracy: {step_accuracy}")
 
         # print training and validation cost and accuracy again
         train_cost, train_accuracy = sess.run([loss, accuracy], feed_dict={x: X_train, y: Y_train})
