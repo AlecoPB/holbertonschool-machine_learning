0b3239dffa18cacc46bfc6249f6668e1aa0f63f7
AlecoPB <ajoaquinpg@gmail.com>
2024-03-13 15:31:18 +0100

    Update 15-model.py

--- supervised_learning/optimization/15-model.py
+++ supervised_learning/optimization/15-model.py
@@ -77,8 +77,8 @@ def model(Data_train, Data_valid, layers, activations, alpha=0.001, beta1=0.9,
                     print("\tCost: {}".format(step_cost))
                     print("\tAccuracy: {}".format(step_accuracy))
 
-        train_cost, train_accuracy = sess.run([cost, accuracy], feed_dict={x: X_train, y: Y_train})
-        valid_cost, valid_accuracy = sess.run([cost, accuracy], feed_dict={x: X_valid, y: Y_valid})
+        # train_cost, train_accuracy = sess.run([cost, accuracy], feed_dict={x: X_train, y: Y_train})
+        # valid_cost, valid_accuracy = sess.run([cost, accuracy], feed_dict={x: X_valid, y: Y_valid})
 
         print("After {} epochs:".format(epochs))
         print("\tTraining Cost: {}".format(train_cost))
