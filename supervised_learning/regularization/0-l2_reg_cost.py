7f7051cb5e6ef08e564bd6b0b879491a9db3cc83
AlecoPB <ajoaquinpg@gmail.com>
2024-03-20 17:17:42 +0100

    Update 0-l2_reg_cost.py

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -2,6 +2,9 @@
 """_summary_
 This is some documentation
 """
+import numpy as np
+
+
 def l2_reg_cost(cost, lambtha, weights, L, m):
     """
     Calculates the cost of a neural network with L2 regularization.
