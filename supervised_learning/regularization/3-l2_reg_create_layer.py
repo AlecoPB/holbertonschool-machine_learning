5b4125346131d93fa283da2f921b0dfa2b2d73c3
AlecoPB <ajoaquinpg@gmail.com>
2024-03-21 17:32:41 +0100

    Update 3-l2_reg_create_layer.py

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -19,12 +19,15 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     Returns:
         _type_: _description_
     """
-    layer = tf.keras.layers.Dense(
-        units=n,
-        activation=activation,
-        kernel_regularizer=tf.keras.regularizers.l2(lambtha)
-    )
+    initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+
+    # Apply L2 regularization to weights
+    regularizer = tf.contrib.layers.l2_regularizer(lambtha)
+
+    # Create fully connected layer
+    layer = tf.layers.Dense(units=n,
+                            activation=activation,
+                            kernel_initializer=initializer,
+                            kernel_regularizer=regularizer)
     
-    output = layer(prev)
-    
-    return output
+    return layer(prev)
