7341fe65c77996791eb07c91fc971b1389b8f27d
AlecoPB <ajoaquinpg@gmail.com>
2024-03-21 16:52:46 +0100

    a

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -23,5 +23,6 @@ def l2_reg_cost(cost):
     #     print(cost(i))
     # for w in weights:
     #     l2_reg_cost += tf.nn.l2_loss(w)
-
+    dense = tf.keras.layers.Dense(3, kernel_regularizer='l2')
+    cost *= dense
     return cost
