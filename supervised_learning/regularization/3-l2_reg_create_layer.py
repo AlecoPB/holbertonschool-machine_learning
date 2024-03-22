c981406ef800952a746eca3ab71ac4443487d41a
AlecoPB <ajoaquinpg@gmail.com>
2024-03-22 10:22:05 +0100

    Update 3-l2_reg_create_layer.py

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -19,9 +19,11 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     Returns:
         _type_: _description_
     """
+    regularizer = tf.keras.regularizers.L2(lambtha)
+    
     layer = tf.layers.Dense(
         units=n,
         activation=activation,
-        kernel_regularizer=tf.keras.regularizers.L2(lambtha))
+        kernel_regularizer='l2')
     
     return layer(prev)
