514c4fe4ef1f51540b4ab250488a69333b5917b5
AlecoPB <ajoaquinpg@gmail.com>
2024-03-23 16:35:51 +0100

    Update 4-dropout_forward_prop.py

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
 
+
 def dropout_forward_prop(X, weights, L, keep_prob):
     """
     Conducts forward propagation using Dropout
@@ -12,13 +13,15 @@ def dropout_forward_prop(X, weights, L, keep_prob):
         keep_prob: float - probability that a node will be kept
 
     Returns:
-        dict: a dictionary containing the outputs of each layer and the dropout mask used on each layer
+        dict: a dictionary containing the outputs 
+        of each layer and the dropout mask used on each layer
     """
     cache = {}
     cache['A0'] = X
-    for l in range(1, L + 1):
-        Z = np.dot(weights['W' + str(l)], cache['A' + str(l - 1)]) + weights['b' + str(l)]
-        if l == L:
+    for lay in range(1, L + 1):
+        Z = np.dot(weights['W' + str(l)],
+                   cache['A' + str(lay - 1)]) + weights['b' + str(lay)]
+        if lay == L:
             A = np.exp(Z) / np.sum(np.exp(Z), axis=0, keepdims=True)
         else:
             A = np.tanh(Z)
@@ -26,6 +29,5 @@ def dropout_forward_prop(X, weights, L, keep_prob):
             A = np.multiply(A, D)
             A /= keep_prob
             cache['D' + str(l)] = D
-        #cache['Z' + str(l)] = Z
-        cache['A' + str(l)] = A
+        cache['A' + str(lay)] = A
     return cache
