445b587c8a620dde14cafa1f71fd915bb0f5c6b3
AlecoPB <ajoaquinpg@gmail.com>
2024-03-23 16:09:04 +0100

    Update 4-dropout_forward_prop.py

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -26,7 +26,6 @@ def dropout_forward_prop(X, weights, L, keep_prob):
             A = np.multiply(A, D)
             A /= keep_prob
             cache['D' + str(l)] = D
-        cache['Z' + str(l)] = Z
         cache['A' + str(l)] = A
         # Print A and D
         print(f'A{l}: {A}')
