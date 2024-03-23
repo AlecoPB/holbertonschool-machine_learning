7200f38e932edd0109ebc7bbbb44d085c45860f6
AlecoPB <ajoaquinpg@gmail.com>
2024-03-23 15:59:23 +0100

    Update 4-dropout_forward_prop.py

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -1,32 +1,23 @@
 #!/usr/bin/env python3
 import numpy as np
 
-def tanh_activation(Z):
-    return np.tanh(Z)
-
-def softmax_activation(Z):
-    expZ = np.exp(Z - np.max(Z))
-    return expZ / expZ.sum(axis=0, keepdims=True)
-
 def dropout_forward_prop(X, weights, L, keep_prob):
-    cache = {}
-    cache["A0"] = X
-    m = X.shape[1]
+    outputs = {}
+    dropout_masks = {}
 
+    # Forward propagation for hidden layers
     for l in range(1, L):
-        Z = np.dot(weights['W' + str(l)], cache['A' + str(l - 1)]) + weights['b' + str(l)]
-        A = tanh_activation(Z)
-        D = np.random.rand(A.shape[0], A.shape[1])
-        D = D < keep_prob
-        A = np.multiply(A, D)
-        A = A / keep_prob
-        cache['Z' + str(l)] = Z
-        cache['D' + str(l)] = D
-        cache['A' + str(l)] = A
+        if l == 1:
+            outputs[l] = np.tanh(np.dot(weights['W' + str(l)], X) + weights['b' + str(l)])
+        else:
+            outputs[l] = np.tanh(np.dot(weights['W' + str(l)], outputs[l-1]) + weights['b' + str(l)])
+        
+        dropout_masks[l] = np.random.rand(outputs[l].shape[0], outputs[l].shape[1]) < keep_prob
+        outputs[l] *= dropout_masks[l]
+        outputs[l] /= keep_prob
 
-    Z = np.dot(weights['W' + str(L)], cache['A' + str(L - 1)]) + weights['b' + str(L)]
-    A = softmax_activation(Z)
-    cache['Z' + str(L)] = Z
-    cache['A' + str(L)] = A
+    # Forward propagation for output layer
+    outputs[L] = np.exp(np.dot(weights['W' + str(L)], outputs[L-1]) + weights['b' + str(L)])
+    outputs[L] /= np.sum(outputs[L], axis=0, keepdims=True)  # softmax
 
-    return cache
+    return outputs, dropout_masks
