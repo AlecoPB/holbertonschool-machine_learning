a9ea93833eaf0549feffab7def4460e187bef8b2
AlecoPB <ajoaquinpg@gmail.com>
2024-03-23 16:38:05 +0100

    Update 4-dropout_forward_prop.py

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""_summary_
+
+Returns:
+    _type_: _description_
+"""
 import numpy as np
 
 
@@ -13,19 +18,20 @@ def dropout_forward_prop(X, weights, L, keep_prob):
         keep_prob: float - probability that a node will be kept
 
     Returns:
-        dict: a dictionary containing the outputs 
+        dict: a dictionary containing the outputs
         of each layer and the dropout mask used on each layer
     """
     cache = {}
     cache['A0'] = X
     for lay in range(1, L + 1):
-        Z = np.dot(weights['W' + str(l)],
+        Z = np.dot(weights['W' + str(lay)],
                    cache['A' + str(lay - 1)]) + weights['b' + str(lay)]
         if lay == L:
             A = np.exp(Z) / np.sum(np.exp(Z), axis=0, keepdims=True)
         else:
             A = np.tanh(Z)
-            D = (np.random.rand(A.shape[0], A.shape[1]) < keep_prob).astype(int)
+            D = (np.random.rand(A.shape[0],
+                                A.shape[1]) < keep_prob).astype(int)
             A = np.multiply(A, D)
             A /= keep_prob
             cache['D' + str(lay)] = D
