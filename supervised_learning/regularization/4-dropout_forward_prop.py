c6e7be9aeb80ba1721611b86872325333a4c3e16
AlecoPB <ajoaquinpg@gmail.com>
2024-03-23 16:07:08 +0100

    Update 4-dropout_forward_prop.py

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -28,35 +28,8 @@ def dropout_forward_prop(X, weights, L, keep_prob):
             cache['D' + str(l)] = D
         cache['Z' + str(l)] = Z
         cache['A' + str(l)] = A
+        # Print A and D
+        print(f'A{l}: {A}')
+        if l < L:
+            print(f'D{l}: {D}')
     return cache
-
-# # Test the function
-# if __name__ == '__main__':
-#     def one_hot(Y, classes):
-#         """convert an array to a one-hot matrix"""
-#         m = Y.shape[0]
-#         one_hot = np.zeros((classes, m))
-#         one_hot[Y, np.arange(m)] = 1
-#         return one_hot
-
-#     np.random.seed(0)
-
-#     weights = {}
-#     weights['W1'] = np.random.randn(256, 784)
-#     weights['b1'] = np.zeros((256, 1))
-#     weights['W2'] = np.random.randn(128, 256)
-#     weights['b2'] = np.zeros((128, 1))
-#     weights['W3'] = np.random.randn(10, 128)
-#     weights['b3'] = np.zeros((10, 1))
-
-#     # MNIST data loading and reshaping
-#     lib= np.load('../data/MNIST.npz')
-#     X_train_3D = lib['X_train']
-#     Y_train = lib['Y_train']
-#     X_train = X_train_3D.reshape((X_train_3D.shape[0], -1)).T
-#     Y_train_oh = one_hot(Y_train, 10)
-
-#     # Forward propagation with dropout
-#     cache = dropout_forward_prop(X_train, weights, 3, 0.8)
-#     for k, v in sorted(cache.items()):
-#         print(k, v)
