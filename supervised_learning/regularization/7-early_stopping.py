a77cedb1853b7ab3e2b6d074a6b842727a24ddb3
AlecoPB <ajoaquinpg@gmail.com>
2024-03-23 18:21:47 +0100

    Update 7-early_stopping.py

--- supervised_learning/regularization/7-early_stopping.py
+++ supervised_learning/regularization/7-early_stopping.py
@@ -17,8 +17,8 @@ def early_stopping(cost, opt_cost, threshold, patience, count):
     int: The updated count.
     """
     # Check if the validation cost has not decreased relative to the optimal validation cost by more than the threshold
-    print(f"{cost} - {opt_cost} = {cost - opt_cost} is {cost - opt_cost >= threshold}")
-    if cost - opt_cost + 0.00001 >= threshold:
+    #print(f"{cost} - {opt_cost} = {cost - opt_cost} is {cost - opt_cost >= threshold}")
+    if opt_cost - cost > threshold:
         print("Se sumó")
         count += 1
     else:
