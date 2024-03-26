9db212777c5d3e4780cf030480ac1d25c8b808de
AlecoPB <ajoaquinpg@gmail.com>
2024-03-26 17:45:20 +0100

    Update 12-test.py

--- supervised_learning/keras/12-test.py
+++ supervised_learning/keras/12-test.py
@@ -8,13 +8,14 @@ import tensorflow.keras as K
 def test_model(network, data, labels, verbose=True):
     """
     Tests a neural network model.
-    
+
     Args:
     - network: the network model to test
     - data: the input data to test the model with
     - labels: the correct one-hot labels of data
-    - verbose: a boolean that determines if output should be printed during the testing process
-    
+    - verbose: a boolean that determines if output
+    should be printed during the testing process
+
     Returns:
     - loss: the loss of the model with the testing data
     - accuracy: the accuracy of the model with the testing data
