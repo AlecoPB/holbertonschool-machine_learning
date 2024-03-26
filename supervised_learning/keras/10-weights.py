41254136a53747f73c96063022cafbbf65adc216
AlecoPB <ajoaquinpg@gmail.com>
2024-03-26 17:19:04 +0100

    Update 10-weights.py

--- supervised_learning/keras/10-weights.py
+++ supervised_learning/keras/10-weights.py
@@ -14,7 +14,7 @@ def save_weights(network, filename, save_format='h5'):
         save_format (str, optional): _description_. Defaults to 'h5'.
     """
     network.save_weights(filename, save_format=save_format)
-    
+
 
 def load_weights(network, filename):
     """_summary_
