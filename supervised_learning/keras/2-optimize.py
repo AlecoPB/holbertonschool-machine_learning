140a79acd660ef14956d2498e432a8cc04a5475f
AlecoPB <ajoaquinpg@gmail.com>
2024-03-26 11:05:41 +0100

    Update 2-optimize.py

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -6,6 +6,14 @@ import tensorflow.keras as K
 
 
 def optimize_model(network, alpha, beta1, beta2):
+    """_summary_
+
+    Args:
+        network: Model to be optimized
+        alpha: Learning rate
+        beta1: Adam first parameter
+        beta2: Adam second parameter
+    """
     optimizer = K.optimizers.Adam(learning_rate=alpha,
                                   beta_1=beta1,
                                   beta_2=beta2)
