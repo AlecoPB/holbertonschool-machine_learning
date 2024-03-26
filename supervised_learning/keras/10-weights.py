be23e95faf679da47af7e3f03a773113984a892f
AlecoPB <ajoaquinpg@gmail.com>
2024-03-26 17:18:17 +0100

    Create 10-weights.py

---
+++ supervised_learning/keras/10-weights.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""_summary_
+This is some documentation
+"""
+import tensorflow.keras as K
+
+
+def save_weights(network, filename, save_format='h5'):
+    """_summary_
+
+    Args:
+        network (_type_): _description_
+        filename (_type_): _description_
+        save_format (str, optional): _description_. Defaults to 'h5'.
+    """
+    network.save_weights(filename, save_format=save_format)
+
+def load_weights(network, filename):
+    """_summary_
+
+    Args:
+        network (_type_): _description_
+        filename (_type_): _description_
+    """
+    network.load_weights(filename)
