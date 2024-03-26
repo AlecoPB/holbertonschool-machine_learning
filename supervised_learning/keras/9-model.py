ea43f39d3dd732efc1becb3955b4a61b66f094ae
AlecoPB <ajoaquinpg@gmail.com>
2024-03-26 17:13:26 +0100

    Update 9-model.py

--- supervised_learning/keras/9-model.py
+++ supervised_learning/keras/9-model.py
@@ -6,7 +6,22 @@ import tensorflow.keras as K
 
 
 def save_model(network, filename):
+    """_summary_
+
+    Args:
+        network (_type_): _description_
+        filename (_type_): _description_
+    """
     network.save(filename)
 
+
 def load_model(filename):
+    """_summary_
+
+    Args:
+        filename (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return K.models.load_model(filename)
