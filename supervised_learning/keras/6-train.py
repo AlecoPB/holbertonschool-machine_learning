37388a4eb89bfaa6377427aaef785be4dd612409
AlecoPB <ajoaquinpg@gmail.com>
2024-03-26 16:11:45 +0100

    Update 6-train.py

--- supervised_learning/keras/6-train.py
+++ supervised_learning/keras/6-train.py
@@ -5,6 +5,9 @@ This is some documentation
 import tensorflow.keras as K
 
 
+def step_decay(epoch, alpha, decay_rate):
+        return alpha / (1 + decay_rate * epoch)
+
 def train_model(network,
                 data,
                 labels,
@@ -13,6 +16,9 @@ def train_model(network,
                 validation_data=None,
                 early_stopping=False,
                 patience=0,
+                learning_rate_decay=False,
+                alpha=0.1,
+                decay_rate=1,
                 verbose=True,
                 shuffle=False):
     """_summary_
@@ -29,12 +35,17 @@ def train_model(network,
     Returns:
         _type_: _description_
     """
+
     callbacks = []
-    if validation_data and early_stopping and patience < epochs:
-        early_stopping_callback =\
-            K.callbacks.EarlyStopping(monitor='val_loss',
-                                      patience=patience)
-        callbacks.append(early_stopping_callback)
+    if validation_data and patience < epochs:
+        if early_stopping:
+            early_stopping_callback = K.callbacks.EarlyStopping(monitor='val_loss', patience=patience)
+            callbacks.append(early_stopping_callback)
+        
+        if learning_rate_decay:
+            lr_decay_callback = K.callbacks.LearningRateScheduler(lambda epoch: step_decay(epoch, alpha, decay_rate), verbose=1)
+            callbacks.append(lr_decay_callback)
+
     history = network.fit(data,
                           labels,
                           batch_size=batch_size,
