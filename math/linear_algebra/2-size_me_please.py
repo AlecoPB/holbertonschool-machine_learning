e2994824387727d28440769f07d5378d50c0b3f9
AlecoPB <ajoaquinpg@gmail.com>
2024-04-16 15:03:03 +0200

    Update 2-size_me_please.py

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -8,9 +8,6 @@ def matrix_shape(matrix):
     else:
         length.append(len(matrix))
         prev_res = matrix_shape(matrix[0])
-        if isinstance(prev_res, int):
-            length.append(prev_res)
-        else:
-            for i in range(0, len(prev_res)):
-                length.append(prev_res[i])
+        for i in range(0, len(prev_res)):
+            length.append(prev_res[i])
         return length
