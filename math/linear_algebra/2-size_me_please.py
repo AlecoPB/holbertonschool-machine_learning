6d36eaa831a608fb11e827ce55646f751a286057
AlecoPB <ajoaquinpg@gmail.com>
2024-04-16 14:58:37 +0200

    Create 2-size_me_please.py

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def matrix_shape(matrix):
+    length = []
+    if not isinstance(matrix[0], list):
+        return len(matrix)
+    else:
+        length.append(len(matrix))
+        prev_res = matrix_shape(matrix[0])
+        if isinstance(prev_res, int):
+            length.append(prev_res)
+        else:
+            for i in range(0, len(prev_res)):
+                length.append(prev_res[i])
+        return length
