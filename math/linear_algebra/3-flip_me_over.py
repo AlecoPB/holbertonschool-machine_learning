60b443735c3753d96de7c4194b32c0c817722f1e
AlecoPB <ajoaquinpg@gmail.com>
2024-04-16 15:45:35 +0200

    Create 3-flip_me_over.py

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+This is some documentation
+"""
+
+
+def matrix_transpose(matrix):
+    """
+    This fucntion returns the tranpose
+    of a 2D matrix
+    """
+    length = len(matrix[0])
+    width = len(matrix)
+    transpose = []
+    for i in range(0, length):
+        transpose.append([])
+        for j in range(0, width):
+            if i >= width:
+                break
+            transpose[i].append(matrix[j][i])
+
+    return transpose
