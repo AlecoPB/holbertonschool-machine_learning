987c03e4b256a3a6e38242e48ba49c3905779788
AlecoPB <ajoaquinpg@gmail.com>
2024-04-17 15:26:37 +0200

    Create 10-ill_use_my_scale.py

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+This is some documentation
+"""
+import numpy as np
+
+
+def np_shape(matrix):
+    """_summary_
+
+    Args:
+        matrix (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    if len(matrix) == 0:
+        return (0,)
+    elif not isinstance(matrix[0], np.ndarray):
+        return (len(matrix),)
+    else:
+        return (len(matrix),) + np_shape(matrix[0])
