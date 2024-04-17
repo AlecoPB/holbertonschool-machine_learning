e22f00b61fedae226ea57f30fb29aa3041ce23d7
AlecoPB <ajoaquinpg@gmail.com>
2024-04-17 15:45:23 +0200

    Create 12-bracin_the_elements.py

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This is some documentation
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """_summary_
+
+    Args:
+        mat1 (_type_): _description_
+        mat2 (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return mat1 + mat2
