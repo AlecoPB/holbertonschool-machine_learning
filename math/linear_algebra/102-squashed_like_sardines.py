275d2c25e3e8632b0afc82245acb0528b3099d64
AlecoPB <ajoaquinpg@gmail.com>
2024-04-23 14:03:08 +0200

    Update 102-squashed_like_sardines.py

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -5,17 +5,29 @@ This is some documentation
 
 
 def cat_matrices(mat1, mat2, axis=0):
-    # Check if the matrices can be concatenated along the given axis
-    if len(mat1[0]) != len(mat2[0]) and axis == 0:
-        return None
-    if len(mat1) != len(mat2) and axis == 1:
-        return None
+    # Base case: if the matrices are 1-D lists, concatenate them
+    if not isinstance(mat1[0], list):
+        if axis == 0:
+            return mat1 + mat2
+        else:
+            return None
 
-    # Concatenate the matrices
-    result = []
-    if axis == 0:
-        result = mat1 + mat2
-    elif axis == 1:
-        for m1, m2 in zip(mat1, mat2):
-            result.append(m1 + m2)
-    return result
+    # Recursive case: if the matrices have multiple dimensions
+    else:
+        # Check if the matrices can be concatenated along the given axis
+        if len(mat1) != len(mat2) and axis != 0:
+            return None
+        if len(mat1[0]) != len(mat2[0]) and axis != 1:
+            return None
+
+        # Concatenate the matrices
+        result = []
+        if axis == 0:
+            result = mat1 + mat2
+        elif axis == 1:
+            for m1, m2 in zip(mat1, mat2):
+                concatenated = cat_matrices(m1, m2, axis=0)
+                if concatenated is None:
+                    return None
+                result.append(concatenated)
+        return result
