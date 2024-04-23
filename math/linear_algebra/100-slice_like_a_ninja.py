4921b0e92bd9a0e0cdccdb48b49173e5d12991e4
AlecoPB <ajoaquinpg@gmail.com>
2024-04-23 11:12:51 +0200

    Update 100-slice_like_a_ninja.py

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -5,17 +5,14 @@ This is some documentation
 
 
 def np_slice(matrix, axes={}):
-    new_arr = []
-    for key, value in axes.items():
-        val1 = value[0]
-        val2 = value[1] if len(value) > 1 else None
-        if matrix.ndim == 1:
-            return matrix[val1:val2]
+    """
+    This is some more docu
+    """
+    slices = []
+    for axis in range(len(matrix.shape)):
+        if axis in axes:
+            slices.append(slice(*axes[axis]))
         else:
-            new_axes = {k: v for k, v in axes.items() if k >= key}
-            for i in range(len(matrix)):
-                temp_arr = np_slice(matrix[i], new_axes)
-                new_arr.append([])
-                for j in temp_arr:
-                    new_arr[i].append(j)
-    return new_arr
+            slices.append(slice(None, None))
+    sliced_matrix = matrix[tuple(slices)]
+    return sliced_matrix
