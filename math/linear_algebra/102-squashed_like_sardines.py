0dd6124e2a229c3f7d38c603767bf1eceb031fae
AlecoPB <ajoaquinpg@gmail.com>
2024-04-23 14:28:14 +0200

    Update 102-squashed_like_sardines.py

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -5,13 +5,37 @@ This is some documentation
 
 
 def cat_matrices(mat1, mat2, axis=0):
-    if axis == 0:
-        if len(mat1[0]) != len(mat2[0]):
-            return None
-        return [row[:] for row in mat1] + [row[:] for row in mat2]
-    elif axis == 1:
-        if len(mat1) != len(mat2):
-            return None
-        return [mat1[i] + mat2[i] for i in range(len(mat1))]
-    else:
-        return None
+  """
+  Concatenates two matrices (ndarrays) along a specific axis.
+
+  Args:
+      mat1: First matrix (ndarray).
+      mat2: Second matrix (ndarray).
+      axis: Axis along which to concatenate (default: 0).
+
+  Returns:
+      A new matrix (ndarray) containing the concatenated matrices, 
+      or None if concatenation is not possible.
+  """
+
+  # Check if matrices are empty (given assumption)
+  if not mat1.size or not mat2.size:
+    return None
+
+  # Validate data type consistency
+  if mat1.dtype != mat2.dtype:
+    return None
+
+  # Validate dimension compatibility (except for concatenation axis)
+  for i in range(len(mat1.shape)):
+    if i != axis and mat1.shape[i] != mat2.shape[i]:
+      return None
+
+  # Concatenate based on axis
+  if axis == 0:
+    return np.concatenate((mat1, mat2))
+  elif axis == 1:
+    return np.concatenate((mat1, mat2), axis=axis)
+  else:
+    # Invalid axis provided
+    return None
