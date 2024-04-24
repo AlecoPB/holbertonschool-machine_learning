769b608e2b30affe62b84daeebff039ac8449664
AlecoPB <ajoaquinpg@gmail.com>
2024-04-24 15:00:57 +0200

    Update 4-frequency.py

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -19,5 +19,5 @@ def frequency():
     plt.xlim(0, 100)
     plt.ylabel('Number of Students')
     plt.ylim(0, 30)
-    plt.hist(student_grades, bins=np.arange(0, 120, 10),
+    plt.hist(student_grades, bins=np.arange(0, 101, 10),
              edgecolor='black')
