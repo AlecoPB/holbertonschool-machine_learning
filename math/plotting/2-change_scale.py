8d4a5b6c5716e69e31eb648038d96b9d6758bfee
AlecoPB <ajoaquinpg@gmail.com>
2024-04-24 13:52:23 +0200

    Create 2-change_scale.py

---
+++ math/plotting/2-change_scale.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This is some documentation
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+def change_scale():
+    """
+    Basic line plot that shows the
+    exponential decay of C-14 
+    """
+    x = np.arange(0, 28651, 5730)
+    r = np.log(0.5)
+    t = 5730
+    y = np.exp((r / t) * x)
+    plt.figure(figsize=(6.4, 4.8))
+
+    # your code here
