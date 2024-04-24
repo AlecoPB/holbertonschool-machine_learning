a9aacda021fe63e138d8c1cb4ae672bd78514820
AlecoPB <ajoaquinpg@gmail.com>
2024-04-24 11:57:12 +0200

    Update 0-line.py

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
+"""
+This is some documentation
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 def line():
-
+    """
+    Basic line plotting
+    """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-    plot = plt.plot(range(0, 10), y)
-    plt.show(plot)
+    plot = plt.plot(np.arange(0, 11), y, 'r-')
+    plt.show()
