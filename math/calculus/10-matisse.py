4a3d2adb131cef5afa7074d65fc0e8e2cc26dcbf
AlecoPB <ajoaquinpg@gmail.com>
2024-04-26 11:57:01 +0200

    Create 10-matisse.py

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This is some documenation
+"""
+
+
+def poly_derivative(poly):
+    """
+    Derivative of a polynomial
+    """
+    if not isinstance(poly, list)\
+       or len(poly) == 0:
+        return None
+    else:
+        d_x = []
+        for i in range(len(poly)):
+            d_x.append(poly[i]*i)
+        if d_x[0] == 0:
+            d_x.pop(0)
+        return d_x
