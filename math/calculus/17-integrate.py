c0f4760c50ef5a240964263d37d1a38cf8c6c2ee
AlecoPB <ajoaquinpg@gmail.com>
2024-04-30 11:38:01 +0200

    Create 17-integrate.py

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+This is some documentation
+"""
+
+
+def poly_integral(poly, C=0):
+    """_summary_
+
+    Args:
+        poly (_type_): _description_
+        C (int, optional): _description_. Defaults to 0.
+
+    Returns:
+        _type_: _description_
+    """
+    if not isinstance(poly, list) or not all(isinstance(coef, (int, float)) for coef in poly) or not isinstance(C, int):
+        return None
+
+    integral = [C]
+    for i, coef in enumerate(poly):
+        if coef == 0:
+            integral.append(0)
+        else:
+            integral_coef = coef / (i + 1)
+            integral.append(int(integral_coef) if integral_coef.is_integer() else integral_coef)
+
+    # Remove trailing zeros
+    while len(integral) > 1 and integral[-1] == 0:
+        integral.pop()
+
+    return integral
