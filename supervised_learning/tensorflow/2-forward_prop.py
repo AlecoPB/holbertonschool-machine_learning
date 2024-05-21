94184a6b32ded1d56a6d0797b063ed5382803fd8
AlecoPB <ajoaquinpg@gmail.com>
2024-05-21 11:56:26 +0200

    a

---
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+This is some documentation
+"""
+import tensorflow.compat.v1 as tf
+create_layer = __import__('1-create_layer').create_layer
+
+
+def forward_prop(x, layer_sizes=[], activations=[]):
+    """
+    Args:
+        x (_type_): _description_
+        layer_sizes (list, optional): _description_. Defaults to [].
+        activations (list, optional): _description_. Defaults to [].
+    """
+    layer_input = x
+    for i in range(len(layer_sizes)):
+        layer_output = create_layer(layer_input,
+                                    layer_sizes[i], activations[i])
+        layer_input = layer_output
+    prediction = layer_output
+    return prediction
---
+++ supervised_learning/tensorflow/4-calculate_loss.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This is some documentation
+"""
+import tensorflow.compat.v1 as tf
+
+
+def calculate_loss(y, y_pred):
+    """
+    Args:
+        y (_type_): Placeholder for the labels of the input data
+        y_pred (_type_): Tensor containing the network’s predictions
+    Returns:
+        A tensor containing the loss of the prediction
+    """
+    # Compute softmax cross-entropy loss
+
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+        labels=y, logits=y_pred))
+    return loss
