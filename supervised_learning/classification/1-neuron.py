7fcd89d5bad84d12ff7cce0f6f1701c241350620
AlecoPB <ajoaquinpg@gmail.com>
2024-05-21 10:39:53 +0200

    Create 1-neuron.py

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+This module defines the Neuron class
+"""
+import numpy as np
+
+
+class Neuron:
+    """
+    This is the Neuron module
+    The __init__ function initializes attributes
+    and checks for errors
+    """
+    def __init__(self, nx):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.normal(loc=0.0, scale=1.0, size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
