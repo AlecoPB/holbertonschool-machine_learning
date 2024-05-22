c991e978bbf97f9678017cd5b3237b2f018cfc06
AlecoPB <ajoaquinpg@gmail.com>
2024-05-22 16:10:11 +0200

    Create 2-precision.py

---
+++ supervised_learning/error_analysis/2-precision.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This is some documentation
+"""
+import numpy as np
+
+
+def precision(confusion):
+    preci = np.zeros(confusion.shape[0])
+    for i in range(confusion.shape[0]):
+        correct = confusion[i][i]
+        total = sum(confusion[i, :])
+        preci[i] = round(correct / (correct + total), 8)
+    return preci
