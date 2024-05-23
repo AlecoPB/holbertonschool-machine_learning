b6a72c6683229f795e66c78221802a4a4026e348
AlecoPB <ajoaquinpg@gmail.com>
2024-05-23 16:58:51 +0200

    Update 4-f1_score.py

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -15,8 +15,8 @@ def f1_score(confusion):
     sens = sensitivity(confusion)
     prec = precision(confusion)
 
-    f1_scores = 2 * (prec * sens) / (prec + sens)
+    f1_scores = (2 * (prec * sens)) / (prec + sens)
     for i in f1_scores:
-        i = np.floor(i)
+        i = round(i)
 
     return f1_scores
