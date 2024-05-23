613d1a74141186905b17dcefbc8d0e1d4e55a47e
AlecoPB <ajoaquinpg@gmail.com>
2024-05-23 16:44:54 +0200

    Create 4-f1_score.py

---
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This is some documentation
+"""
+import numpy as np
+
+
+def f1_score(confusion):
+    """
+    This function computes the f1_score of a confusion matrix
+    """
+    sensitivity = __import__('1-sensitivity').sensitivity
+    precision = __import__('2-precision').precision
+
+    sens = sensitivity(confusion)
+    prec = precision(confusion)
+
+    f1_scores = 2 * (prec * sens) / (prec + sens)
+
+    return f1_scores
