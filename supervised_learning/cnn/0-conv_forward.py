eb7fe99c619214c4147a9067f345f24ce17404b0
AlecoPB <ajoaquinpg@gmail.com>
2024-06-05 14:25:54 +0200

    Update 0-conv_forward.py

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -4,18 +4,10 @@ This is some documentation
 """
 import numpy as np
 
+
 def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     """
     Forward propagation for a convolutional layer.
-
-    Args:
-        A_prev (numpy.ndarray): Output of the previous layer (shape: (m, h_prev, w_prev, c_prev)).
-        W (numpy.ndarray): Kernels for the convolution (shape: (kh, kw, c_prev, c_new)).
-        b (numpy.ndarray): Biases applied to the convolution (shape: (1, 1, 1, c_new)).
-        activation (function): Activation function applied to the convolution.
-        padding (str, optional): Type of padding used ("same" or "valid"). Defaults to "same".
-        stride (tuple, optional): Strides for the convolution (sh, sw). Defaults to (1, 1).
-
     Returns:
         numpy.ndarray: Output of the convolutional layer.
     """
@@ -33,7 +25,12 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
         pw = 0
 
     # Apply padding
-    A_prev_pad = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant', constant_values=(0, 0))
+    A_prev_pad = np.pad(A_prev, ((0, 0),
+                                 (ph, ph),
+                                 (pw, pw),
+                                 (0, 0)),
+                        mode='constant',
+                        constant_values=(0, 0))
 
     # Compute output dimensions
     h_new = int((h_prev + 2 * ph - kh) / sh) + 1
