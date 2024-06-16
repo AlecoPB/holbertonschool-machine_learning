038b82618b056085f13d5362f05f3a4053647301
AlecoPB <ajoaquinpg@gmail.com>
2024-06-16 21:22:52 +0200

    Update 0-inception_block.py

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -3,8 +3,8 @@
 """
 This module implements an inception block for a neural network.
 """
-
 from tensorflow import keras as K
+
 
 def inception_block(A_prev, filters):
     """
@@ -24,12 +24,20 @@ def inception_block(A_prev, filters):
     conv1 = K.layers.Conv2D(F1, (1, 1), activation='relu')(A_prev)
 
     conv3r = K.layers.Conv2D(F3R, (1, 1), activation='relu')(A_prev)
-    conv3 = K.layers.Conv2D(F3, (3, 3), padding='same', activation='relu')(conv3r)
+    conv3 = K.layers.Conv2D(F3,
+                            (3, 3),
+                            padding='same',
+                            activation='relu')(conv3r)
 
     conv5r = K.layers.Conv2D(F5R, (1, 1), activation='relu')(A_prev)
-    conv5 = K.layers.Conv2D(F5, (5, 5), padding='same', activation='relu')(conv5r)
+    conv5 = K.layers.Conv2D(F5,
+                            (5, 5),
+                            padding='same',
+                            activation='relu')(conv5r)
 
-    pool = K.layers.MaxPooling2D((3, 3), strides=(1, 1), padding='same')(A_prev)
+    pool = K.layers.MaxPooling2D((3, 3),
+                                 strides=(1, 1),
+                                 padding='same')(A_prev)
     pool_conv = K.layers.Conv2D(FPP, (1, 1), activation='relu')(pool)
 
     output = K.layers.Concatenate()([conv1, conv3, conv5, pool_conv])
