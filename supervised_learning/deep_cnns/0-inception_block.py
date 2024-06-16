c70b2c8ab02468a26a9236a2c149750744f3ac7c
AlecoPB <ajoaquinpg@gmail.com>
2024-06-16 21:14:40 +0200

    Update 0-inception_block.py

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -1,24 +1,38 @@
 #!/usr/bin/env python3
+
 """
-This is some documentation
+This module implements an inception block for a neural network.
 """
+
 from tensorflow import keras as K
 
-
 def inception_block(A_prev, filters):
+    """
+    Builds an inception block as described in "Going Deeper with Convolutions (2014)".
+
+    Args:
+        A_prev: the output from the previous layer
+        filters: a tuple or list containing F1, F3R, F3, F5R, F5, FPP, respectively
+
+    Returns:
+        The concatenated output of the inception block
+    """
     F1, F3R, F3, F5R, F5, FPP = filters
 
-    conv_1x1 = K.Conv2D(F1, (1, 1), activation='relu', padding='same')(A_prev)
+    conv1 = K.layers.Conv2D(F1, (1, 1), activation='relu')(A_prev)
 
-    conv_3x3_reduce = K.Conv2D(F3R, (1, 1), activation='relu', padding='same')(A_prev)
-    conv_3x3 = K.Conv2D(F3, (3, 3), activation='relu', padding='same')(conv_3x3_reduce)
+    conv3r = K.layers.Conv2D(F3R, (1, 1), activation='relu')(A_prev)
+    conv3 = K.layers.Conv2D(F3, (3, 3), padding='same', activation='relu')(conv3r)
 
-    conv_5x5_reduce = K.Conv2D(F5R, (1, 1), activation='relu', padding='same')(A_prev)
-    conv_5x5 = K.Conv2D(F5, (5, 5), activation='relu', padding='same')(conv_5x5_reduce)
+    conv5r = K.layers.Conv2D(F5R, (1, 1), activation='relu')(A_prev)
+    conv5 = K.layers.Conv2D(F5, (5, 5), padding='same', activation='relu')(conv5r)
 
-    max_pool = K.MaxPooling2D((3, 3), strides=(1, 1), padding='same')(A_prev)
-    max_pool_conv = K.Conv2D(FPP, (1, 1), activation='relu', padding='same')(max_pool)
+    pool = K.layers.MaxPooling2D((3, 3), strides=(1, 1), padding='same')(A_prev)
+    pool_conv = K.layers.Conv2D(FPP, (1, 1), activation='relu')(pool)
 
-    output = K.concatenate([conv_1x1, conv_3x3, conv_5x5, max_pool_conv], axis=-1)
+    output = K.layers.Concatenate()([conv1, conv3, conv5, pool_conv])
 
     return output
+
+if __name__ == "__main__":
+    pass
