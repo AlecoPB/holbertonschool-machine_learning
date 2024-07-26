5f26720dc68f5da42dd8a5b2d46d93ad63ed6817
AlecoPB <ajoaquinpg@gmail.com>
2024-07-26 23:14:47 +0200

    Update 0-pca.py

---
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+This module contains a function to perform PCA on a dataset.
+"""
+import numpy as np
+
+
+def pca(X, var=0.95):
+	"""
+	Performs PCA on a dataset.
+	"""
+	# Compute the covariance matrix
+	cov_matrix = np.dot(X.T, X) / X.shape[0]
+
+	# Perform eigen decomposition
+	eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
+
+	# Sort the eigenvalues and corresponding eigenvectors in descending order
+	sorted_indices = np.argsort(eigenvalues)[::-1]
+	sorted_eigenvalues = eigenvalues[sorted_indices]
+	sorted_eigenvectors = eigenvectors[:, sorted_indices]
+
+	# Compute the cumulative variance
+	cumulative_variance = np.cumsum(sorted_eigenvalues) / np.sum(sorted_eigenvalues)
+
+	# Determine the number of components to maintain the desired variance
+	nd = np.searchsorted(cumulative_variance, var) + 1
+
+	# Select the top nd eigenvectors
+	W = sorted_eigenvectors[:, :nd]
+
+	return W
