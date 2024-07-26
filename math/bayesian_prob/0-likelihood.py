b85858ac03616260c046a0c295485e9eda4673b4
AlecoPB <ajoaquinpg@gmail.com>
2024-07-26 21:29:56 +0200

    Update 0-likelihood.py

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
 This module contains a function to calculate the likelihood of obtaining
-data given various hypothetical probabilities of developing severe side effects.
+data given various hypothetical probabilities
+of developing severe side effects.
 """
-
-
 import numpy as np
+
 
 def likelihood(x, n, P):
     """
