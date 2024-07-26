d983d9fe70712f38a2a3f50fbdcf0d0df601adc7
AlecoPB <ajoaquinpg@gmail.com>
2024-07-26 23:24:01 +0200

    Update 0-pca.py

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -7,28 +7,28 @@ import numpy as np
 
 np.set_printoptions(precision=4, suppress=False, formatter={'float': '{:0.4e}'.format})
 def pca(X, var=0.95):
-	"""
-	Performs PCA on a dataset.
-	"""
-	# Compute the covariance matrix
-	cov_matrix = np.dot(X.T, X) / X.shape[0]
+    """
+    Performs PCA on a dataset.
+    """
+    # Compute the covariance matrix
+    cov_matrix = np.dot(X.T, X) / X.shape[0]
 
-	# Perform eigen decomposition
-	eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
+    # Perform eigen decomposition
+    eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
 
-	# Sort the eigenvalues and corresponding eigenvectors in descending order
-	sorted_indices = np.argsort(eigenvalues)[::-1]
-	sorted_eigenvalues = eigenvalues[sorted_indices]
-	sorted_eigenvectors = eigenvectors[:, sorted_indices]
+    # Sort the eigenvalues and corresponding eigenvectors in descending order
+    sorted_indices = np.argsort(eigenvalues)[::-1]
+    sorted_eigenvalues = eigenvalues[sorted_indices]
+    sorted_eigenvectors = eigenvectors[:, sorted_indices]
 
-	# Compute the cumulative variance
-	cumulative_variance = np.cumsum(sorted_eigenvalues) / np.sum(sorted_eigenvalues)
+    # Compute the cumulative variance
+    cumulative_variance = np.cumsum(sorted_eigenvalues) / np.sum(sorted_eigenvalues)
 
-	# Determine the number of components to maintain the desired variance
-	nd = np.searchsorted(cumulative_variance, var) + 1
+    # Determine the number of components to maintain the desired variance
+    nd = np.searchsorted(cumulative_variance, var) + 1
 
-	# Select the top nd eigenvectors
+    # Select the top nd eigenvectors
     nd = max(nd, 3)
-	W = sorted_eigenvectors[:, :nd]
+    W = sorted_eigenvectors[:, :nd]
 
-	return W
+    return W
