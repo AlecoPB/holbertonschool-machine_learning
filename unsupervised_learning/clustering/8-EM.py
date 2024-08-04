8dbd3660cbec248f154e64ff33fa71fb7ef43266
AlecoPB <ajoaquinpg@gmail.com>
2024-08-04 21:49:56 +0200

    Update 8-EM.py

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -3,46 +3,39 @@
 This is some documentation
 """
 import numpy as np
+initialize = __import__('4-initialize').initialize
+expectation = __import__('6-expectation').expectation
+maximization = __import__('7-maximization').maximization
 
-def expectation_maximization(data, iterations=100, verbose=False):
+def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     """
-    Perform the Expectation-Maximization algorithm on the given data.
-
-    Parameters:
-    data (np.ndarray): The input data for the EM algorithm.
-    iterations (int): The number of iterations to run the algorithm.
-    verbose (bool): If True, print log likelihood at intervals.
-
-    Returns:
-    tuple: The final parameters of the model.
+    Performs the expectation maximization for a GMM.
     """
-    # Initialize parameters
-    n, d = data.shape
-    means = np.random.rand(2, d)
-    covariances = np.array([np.eye(d)] * 2)
-    weights = np.ones(2) / 2
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None, None, None, None
+    if not isinstance(k, int) or k <= 0:
+        return None, None, None, None, None
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None, None, None, None
+    if not isinstance(tol, float) or tol < 0:
+        return None, None, None, None, None
+    if not isinstance(verbose, bool):
+        return None, None, None, None, None
+
+    n, d = X.shape
+    pi, m, S = initialize(X, k)
+    g, l = expectation(X, pi, m, S)
 
     for i in range(iterations):
-        # E-step: compute responsibilities
-        responsibilities = np.zeros((n, 2))
-        for k in range(2):
-            responsibilities[:, k] = weights[k] * multivariate_normal.pdf(data, means[k], covariances[k])
-        responsibilities /= responsibilities.sum(axis=1, keepdims=True)
+        pi, m, S = maximization(X, g)
+        g, new_l = expectation(X, pi, m, S)
 
-        # M-step: update parameters
-        for k in range(2):
-            Nk = responsibilities[:, k].sum()
-            means[k] = (data * responsibilities[:, k][:, np.newaxis]).sum(axis=0) / Nk
-            covariances[k] = (responsibilities[:, k][:, np.newaxis, np.newaxis] * 
-                              (data - means[k]).T @ (data - means[k])) / Nk
-            weights[k] = Nk / n
-
-        # Compute log likelihood
-        log_likelihood = np.sum(np.log(np.sum([weights[k] * multivariate_normal.pdf(data, means[k], covariances[k]) 
-                                               for k in range(2)], axis=0)))
-
-        # Print log likelihood if verbose
         if verbose and (i % 10 == 0 or i == iterations - 1):
-            print(f"Log Likelihood after {i} iterations: {log_likelihood:.5f}")
+            print(f"Log Likelihood after {i} iterations: {new_l:.5f}")
 
-    return means, covariances, weights
+        if abs(new_l - l) <= tol:
+            break
+
+        l = new_l
+
+    return pi, m, S, g, l
