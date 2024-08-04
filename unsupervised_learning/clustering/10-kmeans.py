8a6eacd113cccb5297b819d2a83b0f38ed496d0b
AlecoPB <ajoaquinpg@gmail.com>
2024-08-04 22:09:15 +0200

    Update 10-kmeans.py

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -2,19 +2,19 @@
 """
 This is some documentation
 """
-from sklearn.cluster import KMeans
+import sklearn.cluster
 
 
 def kmeans(X, k):
     """
     Performs K-means on a dataset.
     """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+    if not hasattr(X, "shape") or len(X.shape) != 2:
         return None, None
     if not isinstance(k, int) or k <= 0:
         return None, None
 
-    kmeans = KMeans(n_clusters=k)
+    kmeans = sklearn.cluster.KMeans(n_clusters=k)
     kmeans.fit(X)
     C = kmeans.cluster_centers_
     clss = kmeans.labels_
