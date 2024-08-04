e885c1ee1a892d3d4be22a44b186bf0791cff918
AlecoPB <ajoaquinpg@gmail.com>
2024-08-04 21:48:08 +0200

    Update 8-EM.py

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -4,28 +4,45 @@ This is some documentation
 """
 import numpy as np
 
+def expectation_maximization(data, iterations=100, verbose=False):
+    """
+    Perform the Expectation-Maximization algorithm on the given data.
 
-def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
-    try:
-        initialize = __import__('4-initialize').initialize
-        expectation = __import__('6-expectation').expectation
-        maximization = __import__('7-maximization').maximization
+    Parameters:
+    data (np.ndarray): The input data for the EM algorithm.
+    iterations (int): The number of iterations to run the algorithm.
+    verbose (bool): If True, print log likelihood at intervals.
 
-        pi, m, S = initialize(X, k)
-        l_prev = 0
+    Returns:
+    tuple: The final parameters of the model.
+    """
+    # Initialize parameters
+    n, d = data.shape
+    means = np.random.rand(2, d)
+    covariances = np.array([np.eye(d)] * 2)
+    weights = np.ones(2) / 2
 
-        for i in range(iterations):
-            g, l = expectation(X, pi, m, S)
-            pi, m, S = maximization(X, g)
+    for i in range(iterations):
+        # E-step: compute responsibilities
+        responsibilities = np.zeros((n, 2))
+        for k in range(2):
+            responsibilities[:, k] = weights[k] * multivariate_normal.pdf(data, means[k], covariances[k])
+        responsibilities /= responsibilities.sum(axis=1, keepdims=True)
 
-            if verbose and (i % 10 == 0 or i == iterations - 1):
-                print(f"Log Likelihood after {i} iterations: {l:.5f}")
+        # M-step: update parameters
+        for k in range(2):
+            Nk = responsibilities[:, k].sum()
+            means[k] = (data * responsibilities[:, k][:, np.newaxis]).sum(axis=0) / Nk
+            covariances[k] = (responsibilities[:, k][:, np.newaxis, np.newaxis] * 
+                              (data - means[k]).T @ (data - means[k])) / Nk
+            weights[k] = Nk / n
 
-            if abs(l - l_prev) <= tol:
-                break
+        # Compute log likelihood
+        log_likelihood = np.sum(np.log(np.sum([weights[k] * multivariate_normal.pdf(data, means[k], covariances[k]) 
+                                               for k in range(2)], axis=0)))
 
-            l_prev = l
+        # Print log likelihood if verbose
+        if verbose and (i % 10 == 0 or i == iterations - 1):
+            print(f"Log Likelihood after {i} iterations: {log_likelihood:.5f}")
 
-        return pi, m, S, g, l
-    except Exception:
-        return None, None, None, None, None
+    return means, covariances, weights
