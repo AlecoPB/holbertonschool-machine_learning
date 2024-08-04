9319eac5485992516947a67cf126e4214f6804db
AlecoPB <ajoaquinpg@gmail.com>
2024-08-04 21:22:45 +0200

    Create 7-maximization.py

---
+++ unsupervised_learning/clustering/7-maximization.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+This is some documentation
+"""
+import numpy as np
+
+
+def maximization(X, g):
+    """
+    Calculates the maximization step in the EM algorithm on a GMM.
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None, None
+    if not isinstance(g, np.ndarray) or len(g.shape) != 2:
+        return None, None, None
+    n, d = X.shape
+    k, n_g = g.shape
+    if n != n_g:
+        return None, None, None
+
+    # Calculate the updated priors
+    pi = np.sum(g, axis=1) / n
+
+    # Calculate the updated means
+    m = np.dot(g, X) / np.sum(g, axis=1)[:, np.newaxis]
+
+    # Calculate the updated covariance matrices
+    S = np.zeros((k, d, d))
+    for i in range(k):
+        X_centered = X - m[i]
+        S[i] = np.dot(g[i] * X_centered.T, X_centered) / np.sum(g[i])
+
+    return pi, m, S
