1dbd7f638869715ecc9115d41222f96f2218f13d
AlecoPB <ajoaquinpg@gmail.com>
2024-08-09 00:48:12 +0200

    Create 1-regular.py

---
+++ unsupervised_learning/hmm/1-regular.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module that defines a function called markov_chain
+"""
+import numpy as np
+
+
+def markov_chain(P, s, t=1):
+    """
+    Determines the probability of a markov chain
+    being in a steady state
+    """
+    try:
+        result = s
+        steady = []
+        while result != steady:
+            steady = result
+            result = np.matmul(result, P)
+        return result
+    except:
+        return None
