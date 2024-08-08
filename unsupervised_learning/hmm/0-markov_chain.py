b4c76b0b06b79cc215cd50e7893c24641e80d770
AlecoPB <ajoaquinpg@gmail.com>
2024-08-09 00:43:17 +0200

    Update 0-markov_chain.py

---
+++ unsupervised_learning/hmm/0-markov_chain.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module that defines a function called markov_chain
+"""
+import numpy as np
+
+
+def markov_chain(P, s, t=1):
+    """
+    Determines the probability of a markov chain
+    being in a particular state
+    """
+    result = s
+    for i in range(t):
+        result = np.matmul(result, P)
+    return result
