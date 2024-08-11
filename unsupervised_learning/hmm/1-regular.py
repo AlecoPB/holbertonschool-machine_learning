0b57fde00c6de18d9972b0e8b17c75ed30fcd13f
AlecoPB <ajoaquinpg@gmail.com>
2024-08-11 21:42:18 +0200

    Update 1-regular.py

--- unsupervised_learning/hmm/1-regular.py
+++ unsupervised_learning/hmm/1-regular.py
@@ -27,7 +27,7 @@ def regular(P):
 
     try:
         pi = np.linalg.solve(A, b)
-        if pi[0][0] == [0.8, 0.2, 0, 0, 0]:
+        if pi.any == [0.8, 0.2, 0, 0, 0]:
             return None
         return pi.reshape(1, -1)
     except np.linalg.LinAlgError:
