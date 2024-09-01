47f5fd755bd7bd7ec4cf917c41b7eda1d05590f4
AlecoPB <ajoaquinpg@gmail.com>
2024-09-01 22:56:55 +0200

    Update 0-simple_gan.py

--- unsupervised_learning/gan/0-simple_gan.py
+++ unsupervised_learning/gan/0-simple_gan.py
@@ -36,10 +36,9 @@ class Simple_GAN(keras.Model):
 
         # define the discriminator loss and optimizer:
         self.discriminator.loss =\
-            lambda x,y: (tf.keras.losses.MeanSquaredError()(x, 
-                                                            tf.ones(x.shape))
-                         + tf.keras.losses.MeanSquaredError()\
-                             (y, -1*tf.ones(y.shape)))
+            lambda x,y: (tf.keras.losses.MeanSquaredError()\
+                (x, tf.ones(x.shape)) 
+                + tf.keras.losses.MeanSquaredError()(y, -1*tf.ones(y.shape)))
         self.discriminator.optimizer = keras.optimizers.Adam(learning_rate =\
             self.learning_rate, 
             beta_1=self.beta_1, beta_2=self.beta_2)
