ff63722ecca9ce0c1f9bb0247ee4e648c681342d
AlecoPB <ajoaquinpg@gmail.com>
2024-09-01 23:43:44 +0200

    Update 2-wgan_gp.py

--- unsupervised_learning/gan/2-wgan_gp.py
+++ unsupervised_learning/gan/2-wgan_gp.py
@@ -17,11 +17,11 @@ class WGAN_GP(keras.Model) :
         self.discriminator = discriminator
         self.batch_size = batch_size
         self.disc_iter = disc_iter
-        
+
         self.learning_rate = learning_rate
         self.beta_1 = .3
         self.beta_2 = .9
-        
+
         self.lambda_gp = lambda_gp
         self.dims = self.real_examples.shape
         self.len_dims=tf.size(self.dims)
@@ -31,24 +31,20 @@ class WGAN_GP(keras.Model) :
         for i in range(1,self.len_dims):
             self.scal_shape[i]=1
         self.scal_shape=tf.convert_to_tensor(self.scal_shape)
-        
-        # define the generator loss and optimizer:
-        self.generator.loss  = lambda x: lambda x : -tf.math.reduce_mean(x)          
+
+        self.generator.loss  = lambda x: -tf.math.reduce_mean(x)          
         self.generator.optimizer = keras.optimizers.Adam(learning_rate=self.learning_rate, beta_1=self.beta_1, beta_2=self.beta_2)
         self.generator.compile(optimizer=generator.optimizer , loss=generator.loss )
-        
-        # define the discriminator loss and optimizer:
-        self.discriminator.loss = lambda x, y: lambda x,y : tf.math.reduce_mean(y) - tf.math.reduce_mean(x)
+
+        self.discriminator.loss = lambda x, y: tf.math.reduce_mean(y) - tf.math.reduce_mean(x)
         self.discriminator.optimizer = keras.optimizers.Adam(learning_rate=self.learning_rate, beta_1=self.beta_1, beta_2=self.beta_2)
         self.discriminator.compile(optimizer=discriminator.optimizer, loss=discriminator.loss)
 
-    # generator of real samples of size batch_size
     def get_fake_sample(self, size=None, training=False):
         if not size :
             size= self.batch_size
         return self.generator(self.latent_generator(size), training=training)
 
-    # generator of fake samples of size batch_size
     def get_real_sample(self, size=None):
         if not size :
             size= self.batch_size
@@ -56,13 +52,11 @@ class WGAN_GP(keras.Model) :
         random_indices  = tf.random.shuffle(sorted_indices)[:size]
         return tf.gather(self.real_examples, random_indices)
     
-    # generator of interpolating samples of size batch_size              # <---- New !
     def get_interpolated_sample(self,real_sample,fake_sample):
         u = tf.random.uniform(self.scal_shape)
         v=tf.ones(self.scal_shape)-u
         return u*real_sample+v*fake_sample
     
-    # computing the gradient penalty                                     # <---- New !
     def gradient_penalty(self,interpolated_sample):
         with tf.GradientTape() as gp_tape:
                 gp_tape.watch(interpolated_sample)
@@ -71,7 +65,6 @@ class WGAN_GP(keras.Model) :
         norm = tf.sqrt(tf.reduce_sum(tf.square(grads), axis=self.axis))
         return tf.reduce_mean((norm - 1.0) ** 2)      
      
-         # overloading train_step()    
     def train_step(self,useless_argument):
         discr_loss = 0
         for _ in range(self.disc_iter) :
@@ -91,7 +84,7 @@ class WGAN_GP(keras.Model) :
             # apply gradient descent with respect to new_discr_loss once to the discriminator
             gradients_of_discriminator = disc_tape.gradient(new_discr_loss, self.discriminator.trainable_variables)
             self.discriminator.optimizer.apply_gradients(zip(gradients_of_discriminator, self.discriminator.trainable_variables))
-        
+
         with tf.GradientTape() as gen_tape:
             # get a fake sample 
             fake_sample = self.get_fake_sample(training=True)
@@ -100,6 +93,6 @@ class WGAN_GP(keras.Model) :
         # apply gradient descent to the generator
         gradients_of_generator = gen_tape.gradient(gen_loss, self.generator.trainable_variables)
         self.generator.optimizer.apply_gradients(zip(gradients_of_generator, self.generator.trainable_variables))
-        
+
         return {"discr_loss": discr_loss, "gen_loss": gen_loss, "gp":gp}
     
