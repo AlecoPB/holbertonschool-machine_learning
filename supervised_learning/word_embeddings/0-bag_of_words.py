c8358b2dafc06beac0910b2e402a5a5dc3e61c4f
AlecoPB <ajoaquinpg@gmail.com>
2024-09-18 13:19:11 +0200

    Update 0-bag_of_words.py

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -3,11 +3,18 @@
 Bad of Words
 """
 import numpy as np
+import string
+
 
 def bag_of_words(sentences, vocab=None):
     """
     Creates a bag of words
     """
+    for sentence in sentences:
+        # Lowercase the sentence and remove punctuation
+        sentence = sentence.lower().translate(str.maketrans("", "", string.punctuation))
+        sentences.append(sentence.split())
+
     if vocab is None:
         vocab = set(word for sentence in sentences for word in sentence.split())
     else:
