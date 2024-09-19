01da3149ecb090718a0117719e73a0baa2d63c1d
AlecoPB <ajoaquinpg@gmail.com>
2024-09-19 17:26:22 +0200

    Update 0-bag_of_words.py

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -13,7 +13,7 @@ def bag_of_words(sentences, vocab=None):
     bow = CountVectorizer()
     bow.fit(sentences)
     # features = bow.get_feature_names_out(vocab)
-    features = bow.get_feature_names
+    features = bow.get_feature_names_out()
     features = sorted(features)
     
     bow_features = bow.transform(sentences)
