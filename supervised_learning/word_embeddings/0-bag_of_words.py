dcc67f69d5eb54b35ff9cacd650bdceb0d2563e0
AlecoPB <ajoaquinpg@gmail.com>
2024-09-19 17:22:06 +0200

    Update 0-bag_of_words.py

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -9,8 +9,8 @@ def bag_of_words(sentences, vocab=None):
     """
     Creates a bag of words
     """
-    bow = CountVectorizer(stop_words='english')
-    # bow = CountVectorizer()
+    # bow = CountVectorizer(stop_words='english')
+    bow = CountVectorizer()
     bow.fit(sentences)
     features = bow.get_feature_names_out(vocab)
     features = sorted(features)
@@ -18,4 +18,4 @@ def bag_of_words(sentences, vocab=None):
     bow_features = bow.transform(sentences)
     embeddings = bow_features.toarray()
 
-    return embeddings, features
+    return embeddings, np.array(features)
