937aa1fb85c6aa9fa229c1ee4c919c1b5aa06c29
AlecoPB <ajoaquinpg@gmail.com>
2024-09-19 18:31:59 +0200

    Update 1-tf_idf.py

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -5,18 +5,24 @@ Bad of Words
 from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
 import gensim
+from gensim import corpora
 
 
 def tf_idf(sentences, vocab=None):
     """
     Creates a bag of words
     """
+    # 1. Create Bag of Words (BoW) using CountVectorizer
     bow = CountVectorizer(vocabulary=vocab)
-    bow.fit(sentences)
-    features = bow.get_feature_names_out()
-    if vocab is None:
-        features = sorted(features)
-
-    features = gensim.models.TfidfModel(features)
+    bow_matrix = bow.fit_transform(sentences)
+    feature_names = bow.get_feature_names_out()
     
-    return None, features
+    # 2. Convert BoW matrix to Gensim's BoW format
+    corpus = [list(zip(range(len(doc)), doc)) for doc in bow_matrix.toarray()]
+    
+    # 3. Apply TF-IDF transformation using gensim
+    gensim_dictionary = corpora.Dictionary.from_corpus(corpus, id2word=dict(enumerate(feature_names)))
+    tfidf_model = gensim.models.TfidfModel(corpus)
+    tfidf_corpus = [tfidf_model[doc] for doc in corpus]
+    
+    return tfidf_corpus, feature_names
