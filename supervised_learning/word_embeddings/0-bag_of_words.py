3c522c4e4850124cee7f0d8e18dfe9f55f168edb
AlecoPB <ajoaquinpg@gmail.com>
2024-09-19 13:59:56 +0200

    Update 0-bag_of_words.py

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -5,14 +5,14 @@ Bad of Words
 from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
 
-
 def bag_of_words(sentences, vocab=None):
     """
     Creates a bag of words
     """
     bow = CountVectorizer(stop_words='english')
     bow.fit(sentences)
-    features = bow.get_feature_names_out(vocab)   
+    features = bow.get_feature_names_out(vocab)
+    features = sorted(features)
     
     bow_features = bow.transform(features)
     embeddings = bow_features.toarray()
