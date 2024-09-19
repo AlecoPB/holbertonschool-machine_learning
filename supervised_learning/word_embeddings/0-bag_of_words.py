a54d46540f30057892501b04b62d35f544f18fc4
AlecoPB <ajoaquinpg@gmail.com>
2024-09-19 13:41:53 +0200

    Update 0-bag_of_words.py

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -2,21 +2,16 @@
 """
 Bad of Words
 """
-import numpy as np
-import string
-import re
+from sklearn.feature_extraction.text import CountVectorizer
+import nltk
 
 
 def bag_of_words(sentences, vocab=None):
     """
     Creates a bag of words
     """
-    tokenized_sentences = []
-    for sentence in sentences:
-        # Lowercase the sentence and remove punctuation
-        sentence = sentence.lower().translate(str.maketrans("", "", string.punctuation))
-        sentence = re.sub(r"'s\b", "", sentence)
-        tokenized_sentences.append(sentence.split())
+    str_buffer=" ".join(sentences)
+    tokenized_sentences = nltk.word_tokenize(str_buffer)
 
     if vocab is None:
         vocab = set(word for sentence in tokenized_sentences for word in sentence)
@@ -24,14 +19,8 @@ def bag_of_words(sentences, vocab=None):
         vocab = set(vocab)
     
     vocab = sorted(vocab)
-    # features = np.array(vocab)
-    features = vocab
-    embeddings = np.zeros((len(sentences), len(vocab)), dtype=int)
-    
-    for i, sentence in enumerate(sentences):
-        words = sentence.split()
-        for word in words:
-            if word in vocab:
-                embeddings[i, vocab.index(word)] += 1
 
-    return embeddings, features
+    vectorizer_ng2=CountVectorizer(ngram_range=range(1, 3), stop_words='english')
+    embeddings = vectorizer_ng2.fit_transform(vocab)
+
+    return embeddings, vocab
