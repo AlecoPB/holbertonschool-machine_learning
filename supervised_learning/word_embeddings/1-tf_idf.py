f81baa909d6af1314a40ea2ac6d8db39af3e862d
AlecoPB <ajoaquinpg@gmail.com>
2024-09-19 18:07:11 +0200

    Create 1-tf_idf.py

---
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Bad of Words
+"""
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+import gensim
+
+
+def bag_of_words(sentences, vocab=None):
+    """
+    Creates a bag of words
+    """
+    bow = CountVectorizer(vocabulary=vocab)
+    bow.fit(sentences)
+    features = bow.get_feature_names_out()
+    if vocab is None:
+        features = sorted(features)
+
+    features = gensim.models.TfidfModel(features)
+    
+    return None, features
