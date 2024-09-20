a78056e1d2edd1dd22bd9019c96495884c3373fd
AlecoPB <ajoaquinpg@gmail.com>
2024-09-20 11:36:12 +0200

    Update 0-uni_bleu.py

--- supervised_learning/nlp_metrics/0-uni_bleu.py
+++ supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -13,14 +13,14 @@ def uni_bleu(references, sentence):
     reference_ngrams = {}
     sentence_ngrams = Counter(sentence)
     for i in range(len(references)):
-        reference_ngrams[string(i)] = Counter(references[i])
+        reference_ngrams[i] = Counter(references[i])
 
     match_list = []
     for i in range(len(reference_ngrams)):
-        match_list.append(sum((sentence_ngrams & reference_ngrams[string(i)]).values()))
+        match_list.append(sum((sentence_ngrams & reference_ngrams[i]).values()))
 
     match = max(match_list)
-    possible_ngrams = sum(reference_ngrams[string(match_list.index(match))].values())
+    possible_ngrams = sum(reference_ngrams[match_list.index(match)].values())
 
     precision = match / possible_ngrams if possible_ngrams > 0 else 0
     
