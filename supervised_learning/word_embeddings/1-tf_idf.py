0591a5bd411323e444e744c9a43e3887ba7d2024
AlecoPB <ajoaquinpg@gmail.com>
2024-09-21 20:27:13 +0200

    Update 1-tf_idf.py

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -9,16 +9,6 @@ import numpy as np
 def tf_idf(sentences, vocab=None):
     """
     Creates a TF-IDF embedding matrix.
-
-    Args:
-        sentences (list of str): List of sentences to analyze.
-        vocab (list of str or None): List of vocabulary words to use for the analysis.
-                                     If None, all words within sentences are used.
-
-    Returns:
-        embeddings (numpy.ndarray): Matrix of shape (s, f) containing the TF-IDF embeddings,
-                                    where s is the number of sentences and f is the number of features.
-        features (numpy.ndarray): Sorted list of the features used for embeddings.
     """
     # Initialize TfidfVectorizer with the given vocab
     vectorizer = TfidfVectorizer(vocabulary=vocab)
