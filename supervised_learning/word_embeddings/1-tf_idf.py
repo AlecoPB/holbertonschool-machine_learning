fe0f9a4809f79b2dfb79f55770408e55edf085d3
AlecoPB <ajoaquinpg@gmail.com>
2024-09-21 20:30:29 +0200

    Update 1-tf_idf.py

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -12,14 +12,14 @@ def tf_idf(sentences, vocab=None):
     """
     # Initialize TfidfVectorizer with the given vocab
     vectorizer = TfidfVectorizer(vocabulary=vocab)
-    
+
     # Fit the model and transform the sentences to a TF-IDF representation
     vectorizer.fit(sentences)
     tfidf_features = vectorizer.transform(sentences)
-    
+
     # Get the feature names (vocabulary) used in the transformation
     features = vectorizer.get_feature_names_out()
-    
+
     # Sort the features alphabetically if vocab is None
     if vocab is None:
         features = sorted(features)
