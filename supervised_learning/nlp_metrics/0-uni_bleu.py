e4145f2a94b13b2528298f39478c9630237f9399
AlecoPB <ajoaquinpg@gmail.com>
2024-09-22 00:22:11 +0200

    Update 0-uni_bleu.py

--- supervised_learning/nlp_metrics/0-uni_bleu.py
+++ supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -13,9 +13,16 @@ def uni_bleu(references, sentence):
     # Create a set of unique words in the sentence
     sentence_words = set(sentence)
     
+    max_ref_len = 0
     # Count the number of words in the sentence that appear in the references
     match_count = 0
     for reference in references:
+        # Calculate the length of the current reference
+        ref_len = len(reference)
+
+        # Update the maximum reference length
+        max_ref_len = max(max_ref_len, ref_len)
+
         reference_words = set(reference)
         match_count += len(sentence_words & reference_words)
     
@@ -23,14 +30,11 @@ def uni_bleu(references, sentence):
     precision = match_count / word_count
     
     # Calculate brevity penalty
-    ref_lengths = [len(ref) for ref in references]
-    closest_ref_length = min(ref_lengths, key=lambda ref_len: (abs(ref_len - word_count), ref_len))
-    if word_count > closest_ref_length:
-        brevity_penalty = 1
-    else:
-        brevity_penalty = word_count / closest_ref_length
+    # ref_lengths = [len(ref) for ref in references]
+    # closest_ref_length = min(ref_lengths, key=lambda ref_len: (abs(ref_len - word_count), ref_len))
+    bp = 1 if word_count <= max_ref_len else math.exp(1 - max_ref_len / word_count)
     
     # Calculate unigram BLEU score
-    bleu_score = brevity_penalty * precision
+    bleu_score = bp * precision
     
     return bleu_score
