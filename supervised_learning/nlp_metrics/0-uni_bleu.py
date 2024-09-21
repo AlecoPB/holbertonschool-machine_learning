4344e160ced28ebb8d5fba466de9891688f25710
AlecoPB <ajoaquinpg@gmail.com>
2024-09-22 00:25:59 +0200

    Update 0-uni_bleu.py

--- supervised_learning/nlp_metrics/0-uni_bleu.py
+++ supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -7,34 +7,37 @@ import math
 
 
 def uni_bleu(references, sentence):
+    """
+    Calculates the unigram BLEU score for a sentence.
+    
+    :param references: list of reference translations, each reference translation is a list of words
+    :param sentence: list containing the model proposed sentence
+    :return: unigram BLEU score
+    """
     # Count the number of words in the sentence
     word_count = len(sentence)
     
     # Create a set of unique words in the sentence
     sentence_words = set(sentence)
     
-    max_ref_len = 0
-    # Count the number of words in the sentence that appear in the references
+    # Count the number of words in the sentence that are in the references
     match_count = 0
-    for reference in references:
-        # Calculate the length of the current reference
-        ref_len = len(reference)
-
-        # Update the maximum reference length
-        max_ref_len = max(max_ref_len, ref_len)
-
-        reference_words = set(reference)
-        match_count += len(sentence_words & reference_words)
+    for ref in references:
+        ref_words = set(ref)
+        match_count += len(sentence_words & ref_words)
     
     # Calculate precision
     precision = match_count / word_count
     
     # Calculate brevity penalty
-    # ref_lengths = [len(ref) for ref in references]
-    # closest_ref_length = min(ref_lengths, key=lambda ref_len: (abs(ref_len - word_count), ref_len))
-    bp = 1 if word_count <= max_ref_len else math.exp(1 - max_ref_len / word_count)
+    ref_lengths = [len(ref) for ref in references]
+    closest_ref_length = min(ref_lengths, key=lambda ref_len: (abs(ref_len - word_count), ref_len))
+    if word_count > closest_ref_length:
+        brevity_penalty = 1
+    else:
+        brevity_penalty = word_count / closest_ref_length
     
     # Calculate unigram BLEU score
-    bleu_score = bp * precision
+    bleu_score = brevity_penalty * precision
     
     return bleu_score
