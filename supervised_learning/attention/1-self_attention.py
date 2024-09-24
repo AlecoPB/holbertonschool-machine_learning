aaec4d162ee3e5a0b3bf802f70183d66ce0ff652
AlecoPB <ajoaquinpg@gmail.com>
2024-09-24 11:59:55 +0200

    Update 1-self_attention.py

--- supervised_learning/attention/1-self_attention.py
+++ supervised_learning/attention/1-self_attention.py
@@ -13,24 +13,24 @@ class SelfAttention(tf.keras.layers.Layer):
         super(SelfAttention, self).__init__()
 
         # Public instance attributes (Dense layers)
-        self.W = tf.keras.layers.Dense(units)  # To apply to the previous decoder hidden state
-        self.U = tf.keras.layers.Dense(units)  # To apply to the encoder hidden states
-        self.V = tf.keras.layers.Dense(1)      # To apply to the tanh of W and U outputs
+        self.W = tf.keras.layers.Dense(units)
+        self.U = tf.keras.layers.Dense(units)
+        self.V = tf.keras.layers.Dense(1)
 
     def call(self, s_prev, hidden_states):
         """
         Call method that returns context vector and attention weights
         """
         # Expand the s_prev shape to match the hidden states for broadcasting
-        s_prev_expanded = tf.expand_dims(s_prev, axis=1)  # Shape: (batch, 1, units)
+        s_prev_expanded = tf.expand_dims(s_prev, axis=1)
 
         # Apply W to s_prev and U to hidden_states
-        score = self.V(tf.nn.tanh(self.W(s_prev_expanded) + self.U(hidden_states)))  # Shape: (batch, input_seq_len, 1)
+        score = self.V(tf.nn.tanh(self.W(s_prev_expanded) + self.U(hidden_states)))
 
         # Calculate attention weights with softmax
-        weights = tf.nn.softmax(score, axis=1)  # Shape: (batch, input_seq_len, 1)
+        weights = tf.nn.softmax(score, axis=1)
 
         # Compute context vector as the weighted sum of hidden_states
-        context = tf.reduce_sum(weights * hidden_states, axis=1)  # Shape: (batch, units)
+        context = tf.reduce_sum(weights * hidden_states, axis=1)
 
         return context, weights
