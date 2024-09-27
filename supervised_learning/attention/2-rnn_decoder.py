2e245ff9bd3e246ab54593f6a42563a83d2ae121
AlecoPB <ajoaquinpg@gmail.com>
2024-09-27 10:28:32 +0200

    Update 2-rnn_decoder.py

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -25,12 +25,15 @@ class RNNDecoder(tf.keras.layers.Layer):
         x = self.embedding(x)  # Shape: (batch, 1, embedding_dim)
 
         # Step 3: Concatenate the context vector with the embedded input word
-        x = tf.concat([context, x], axis=-1)
+        x = tf.concat([tf.expand_dims(context, 1), x], axis=-1)
 
         # Step 4: Pass the concatenated input through the GRU layer
         output, s = self.gru(x, initial_state=s_prev)
 
         # Step 5: Pass the GRU output through the Dense layer to get the final predicted word (logits)
         y = self.F(output)  # Shape: (batch, 1, vocab)
+
+        # Step 6: Remove the sequence dimension (1) from the output y
+        y = tf.squeeze(y, axis=1)  # Shape: (batch, vocab) 
 
         return y, s
