9a064b63e17d25257b5cd897edccfcf76fddb42e
AlecoPB <ajoaquinpg@gmail.com>
2024-09-29 23:02:47 +0200

    Create 7-transformer_encoder_block.py

---
+++ supervised_learning/attention/7-transformer_encoder_block.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+This is some documentation
+"""
+import tensorflow as tf
+# from tensorflow.keras.layers import Dense, Dropout, LayerNormalization
+MultiHeadAttention = __import__('6-multihead_attention').MultiHeadAttention
+
+
+class EncoderBlock(tf.keras.layers.Layer):
+    """
+    Encoder block class
+    """
+    def __init__(self, dm, h, hidden, drop_rate=0.1):
+        super(EncoderBlock, self).__init__()
+        self.mha = MultiHeadAttention(dm, h)
+        self.dense_hidden = tf.kears.layers.Dense(hidden, activation='relu')
+        self.dense_output = tf.kears.layers.Dense(dm)
+        self.layernorm1 = tf.kears.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = tf.kears.layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = tf.kears.layers.Dropout(drop_rate)
+        self.dropout2 = tf.kears.layers.Dropout(drop_rate)
+
+    def call(self, x, training, mask=None):
+        """
+        Call function
+        """
+        # Apply multi-head attention
+        attn_output, _ = self.mha(x, x, x, mask)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layernorm1(x + attn_output)
+
+        # Apply the feed-forward network
+        dense_hidden_output = self.dense_hidden(out1)
+        dense_output = self.dense_output(dense_hidden_output)
+        dense_output = self.dropout2(dense_output, training=training)
+        out2 = self.layernorm2(out1 + dense_output)
+
+        return out2
