eec78f3c41651919d6fb5d1a1c56bca7df236b89
AlecoPB <ajoaquinpg@gmail.com>
2024-09-29 23:13:49 +0200

    Update 9-transformer_encoder.py

--- supervised_learning/attention/9-transformer_encoder.py
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -8,13 +8,19 @@ EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
 
 
 class Encoder(tf.keras.layers.Layer):
-    def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len, drop_rate=0.1):
+    def __init__(self, N, dm, h, hidden,
+                 input_vocab, max_seq_len,
+                 drop_rate=0.1):
         super(Encoder, self).__init__()
         self.N = N
         self.dm = dm
-        self.embedding = tf.keras.layers.Embedding(input_dim=input_vocab, output_dim=dm)
+        self.embedding = tf.keras.layers.Embedding(input_dim=input_vocab,
+                                                   output_dim=dm)
         self.positional_encoding = positional_encoding(max_seq_len, dm)
-        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate) for _ in range(N)]
+        self.blocks = [EncoderBlock(dm,
+                                    h,
+                                    hidden,
+                                    drop_rate) for _ in range(N)]
         self.dropout = tf.keras.layers.Dropout(drop_rate)
 
     def call(self, x, training, mask):
