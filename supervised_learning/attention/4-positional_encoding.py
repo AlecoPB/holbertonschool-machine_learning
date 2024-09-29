90e26d915324c5662880f979a92135b931b5bc3e
AlecoPB <ajoaquinpg@gmail.com>
2024-09-29 22:52:16 +0200

    Update 4-positional_encoding.py

--- supervised_learning/attention/4-positional_encoding.py
+++ supervised_learning/attention/4-positional_encoding.py
@@ -10,10 +10,17 @@ def positional_encoding(max_seq_len, dm):
     Calculate the positional encoding for a transformer.
     """
     pe = np.zeros((max_seq_len, dm))
-    for pos in range(max_seq_len):
-        for i in range(dm):
-            if i % 2 == 0:
-                pe[pos, i] = np.sin(pos / (10000 ** (i // 2 / dm)))
-            else:
-                pe[pos, i] = np.cos(pos / (10000 ** ((i - 1) // 2 / dm)))
+
+    # Create an array of position indices
+    position = np.arange(max_seq_len)[:, np.newaxis]
+
+    # Create an array of dimension indices
+    div_term = np.exp(np.arange(0, dm, 2) * -(np.log(10000.0) / dm))
+
+    # Apply the sine function to even indices in the array; 2i
+    pe[:, 0::2] = np.sin(position * div_term)
+
+    # Apply the cosine function to odd indices in the array; 2i+1
+    pe[:, 1::2] = np.cos(position * div_term)
+
     return pe
