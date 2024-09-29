f33d6d24fb2cae89face5ed479b2e7012ff4f9ba
AlecoPB <ajoaquinpg@gmail.com>
2024-09-29 23:12:35 +0200

    Create 9-transformer_encoder.py

---
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+This is some documentation
+"""
+import tensorflow as tf
+positional_encoding = __import__('4-positional_encoding').positional_encoding
+EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
+
+
+class Encoder(tf.keras.layers.Layer):
+    def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len, drop_rate=0.1):
+        super(Encoder, self).__init__()
+        self.N = N
+        self.dm = dm
+        self.embedding = tf.keras.layers.Embedding(input_dim=input_vocab, output_dim=dm)
+        self.positional_encoding = positional_encoding(max_seq_len, dm)
+        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate) for _ in range(N)]
+        self.dropout = tf.keras.layers.Dropout(drop_rate)
+
+    def call(self, x, training, mask):
+        seq_len = tf.shape(x)[1]
+
+        # Generate the embeddings and add the positional encodings
+        x = self.embedding(x)  # (batch, input_seq_len, dm)
+        x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
+        x += self.positional_encoding[:seq_len]
+
+        # Apply dropout to the positional encodings
+        x = self.dropout(x, training=training)
+
+        # Pass the input through each encoder block
+        for block in self.blocks:
+            x = block(x, training, mask)
+
+        return x
