9d20dc1e48a2a5bee343340f56bb5bebba93cf94
AlecoPB <ajoaquinpg@gmail.com>
2024-09-29 23:06:05 +0200

    Update 7-transformer_encoder_block.py

--- supervised_learning/attention/7-transformer_encoder_block.py
+++ supervised_learning/attention/7-transformer_encoder_block.py
@@ -13,12 +13,12 @@ class EncoderBlock(tf.keras.layers.Layer):
     def __init__(self, dm, h, hidden, drop_rate=0.1):
         super(EncoderBlock, self).__init__()
         self.mha = MultiHeadAttention(dm, h)
-        self.dense_hidden = tf.kears.layers.Dense(hidden, activation='relu')
-        self.dense_output = tf.kears.layers.Dense(dm)
-        self.layernorm1 = tf.kears.layers.LayerNormalization(epsilon=1e-6)
-        self.layernorm2 = tf.kears.layers.LayerNormalization(epsilon=1e-6)
-        self.dropout1 = tf.kears.layers.Dropout(drop_rate)
-        self.dropout2 = tf.kears.layers.Dropout(drop_rate)
+        self.dense_hidden = tf.keras.layers.Dense(hidden, activation='relu')
+        self.dense_output = tf.keras.layers.Dense(dm)
+        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = tf.keras.layers.Dropout(drop_rate)
+        self.dropout2 = tf.keras.layers.Dropout(drop_rate)
 
     def call(self, x, training, mask=None):
         """
