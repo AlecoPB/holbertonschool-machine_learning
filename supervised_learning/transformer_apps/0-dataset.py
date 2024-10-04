9c55a27e1fd171a6d8126259e1d91c265978f419
AlecoPB <ajoaquinpg@gmail.com>
2024-10-04 10:25:07 +0200

    Update 0-dataset.py

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -25,26 +25,9 @@ class Dataset:
         """
         Creates sub-word tokenizers for the dataset.
         """
-        tokenizer_pt = transformers.BertTokenizer.from_pretrained('neuralmind/bert-base-portuguese-cased')
-        tokenizer_en = transformers.BertTokenizer.from_pretrained('bert-base-uncased')
+        tokenizer_pt = transformers.BertTokenizer.\
+            from_pretrained('neuralmind/bert-base-portuguese-cased', max_length = 2**13)
+        tokenizer_en = transformers.BertTokenizer.\
+            from_pretrained('bert-base-uncased', max_length = 2**13)
 
-        # Get the original vocabulary
-        vocab_pt = list(tokenizer_pt.vocab.keys())
-        vocab_en = list(tokenizer_en.vocab.keys())
-
-        # Define the desired vocabulary size
-        vocab_size = 2**13
-
-        # Filter out less frequent tokens
-        vocab_pt = vocab_pt[:vocab_size]
-        vocab_en = vocab_en[:vocab_size]
-
-        # Create a new tokenizer with the reduced vocabulary
-        tokenizer_pt_reduced = transformers.BertTokenizer(tokenizer_pt.vocab, tokenizer_pt.special_tokens_map)
-        tokenizer_en_reduced = transformers.BertTokenizer(tokenizer_en.vocab, tokenizer_en.special_tokens_map)
-
-        # Update the tokenizers with the reduced vocabulary
-        tokenizer_pt_reduced.add_tokens(vocab_pt)
-        tokenizer_en_reduced.add_tokens(vocab_en)
-
-        return tokenizer_pt_reduced, tokenizer_en_reduced
+        return tokenizer_pt, tokenizer_en
