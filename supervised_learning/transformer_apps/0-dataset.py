af3f6e4f266435ee4548bdc160f026e66f027c62
AlecoPB <ajoaquinpg@gmail.com>
2024-10-04 11:15:15 +0200

    Update 0-dataset.py

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python3
 """
-Dataset module for loading and preparing a dataset for machine translation.
+Transformer Applications project
+By Ced
 """
 import tensorflow_datasets as tfds
 import transformers
 
 
-class Dataset:
+class Dataset():
     """
-    Dataset class to load and prep a dataset for machine translation.z
+    this class loads and preps a dataset for machine translation
     """
-
     def __init__(self):
         """
-        Initializes the Dataset instance.
+        Class constructor
         """
         self.data_train = tfds.load('ted_hrlr_translate/pt_to_en', split='train', as_supervised=True)
         self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en', split='validation', as_supervised=True)
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(self.data_train)
-
+        
     def tokenize_dataset(self, data):
         """
-        Creates sub-word tokenizers for the dataset.
+        Instance method
         """
-        tokenizer_pt = transformers.BertTokenizer.\
-            from_pretrained('neuralmind/bert-base-portuguese-case')
-        tokenizer_en = transformers.BertTokenizer.\
+
+        tokenizer_pt = transformers.AutoTokenizer.\
+            from_pretrained('neuralmind/bert-base-portuguese-cased')
+        tokenizer_en = transformers.AutoTokenizer.\
             from_pretrained('bert-base-uncased')
 
         def get_training_corpus_en():
@@ -36,8 +37,20 @@ class Dataset:
             for pt, _ in data:
                 yield pt.numpy().decode('utf-8')
 
+        #  How to tokenize a sentence
+        #  sentences = "i love you madly"
+        #  tokens = tokenizer_en.tokenize(sentences)
+        #  print(tokens)
+
         tokenizer_pt = tokenizer_pt.\
-            train_new_from_iterator(get_training_corpus_pt(), vocab_size=2**13)
+            train_new_from_iterator(get_training_corpus_pt(), vocab_size=8192)
         tokenizer_en = tokenizer_en.\
-            train_new_from_iterator(get_training_corpus_en(), vocab_size=2**13)
+            train_new_from_iterator(get_training_corpus_en(), vocab_size=8192)
         return tokenizer_pt, tokenizer_en
+
+    def encode(self, pt, en):
+        """
+        Instance method
+        """
+        print("encode", pt, en)
+        return None
