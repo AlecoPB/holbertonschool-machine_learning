2b88767ac1c932d0bf1e6e6280893db04d9bda53
AlecoPB <ajoaquinpg@gmail.com>
2024-10-04 11:04:51 +0200

    Update 0-dataset.py

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -23,9 +23,21 @@ class Dataset:
         """
         Creates sub-word tokenizers for the dataset.
         """
-        tokenizer_pt = transformers.BertTokenizer.\
-            from_pretrained('neuralmind/bert-base-portuguese-cased', max_length = 2**13)
-        tokenizer_en = transformers.BertTokenizer.\
-            from_pretrained('bert-base-uncased', max_length = 2**13)
+        tokenizer_pt = transformers.AutoTokenizer.\
+            from_pretrained('neuralmind/bert-base-portuguese-case')
+        tokenizer_en = transformers.AutoTokenizer.\
+            from_pretrained('bert-base-uncased')
 
+        def get_training_corpus_en():
+            for _, en in data:
+                yield en.numpy().decode('utf-8')
+
+        def get_training_corpus_pt():
+            for pt, _ in data:
+                yield pt.numpy().decode('utf-8')
+
+        tokenizer_pt = tokenizer_pt.\
+            train_new_from_iterator(get_training_corpus_pt(), vocab_size=2**13)
+        tokenizer_en = tokenizer_en.\
+            train_new_from_iterator(get_training_corpus_en(), vocab_size=2**13)
         return tokenizer_pt, tokenizer_en
