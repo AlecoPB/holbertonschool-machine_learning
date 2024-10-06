5b72612ce11660f41a8a30c81b8f55c3bf4d533d
AlecoPB <ajoaquinpg@gmail.com>
2024-10-06 19:45:40 +0200

    Update 2-dataset.py

--- supervised_learning/transformer_apps/2-dataset.py
+++ supervised_learning/transformer_apps/2-dataset.py
@@ -59,11 +59,11 @@ class Dataset:
         - pt_tokens: tf.Tensor containing the tokenized Portuguese sentence.
         - en_tokens: tf.Tensor containing the tokenized English sentence.
         """
-        # Apply encode using tf.py_function to make it TensorFlow-friendly
+        # Apply encode using tf.py_function, making sure the output is int64
         pt_tokens, en_tokens = tf.py_function(self.encode, [pt, en], [tf.int64, tf.int64])
 
         # Set shape of the tensors after tokenization
-        # pt_tokens.set_shape([None])
-        # en_tokens.set_shape([None])
+        pt_tokens.set_shape([None])
+        en_tokens.set_shape([None])
 
         return pt_tokens, en_tokens
