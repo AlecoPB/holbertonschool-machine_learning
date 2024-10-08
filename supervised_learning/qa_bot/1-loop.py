32098d20dafabb9e0d8fc996bb8505f606092ad5
AlecoPB <ajoaquinpg@gmail.com>
2024-10-08 16:30:11 +0200

    Update 1-loop.py

---
+++ supervised_learning/qa_bot/1-loop.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+while True:
+    Q = input("Q:")
+    if Q.lower() != "exit" or "quit" or "goodbye":
+        print("A")
+
+    else:
+        print("A: Goodbye")
+        break
