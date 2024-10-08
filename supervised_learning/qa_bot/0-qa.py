29f8ec0e924d471c1454e391bcf22ad637512c25
AlecoPB <ajoaquinpg@gmail.com>
2024-10-08 13:54:13 +0200

    Update 0-qa.py

--- supervised_learning/qa_bot/0-qa.py
+++ supervised_learning/qa_bot/0-qa.py
@@ -21,22 +21,33 @@ def question_answer(question, reference):
     #Tokenize the input text
     inputs = tokenizer.encode_plus(question,
                                    reference,
-                                   return_tensors='tf')
+                                   return_tensors='tf',
+                                   return_attention_mask=True)
 
     input_ids = inputs['input_ids']
     token_type_ids = inputs['token_type_ids']
+    attention_mask = inputs['attention_mask']
 
     #Pass inputs to the model
-    outputs = model([input_ids, token_type_ids])
+    outputs = model([input_ids, attention_mask, token_type_ids])
 
-    start_logits, end_logits = outputs[0], outputs[1]
+    #Extract start and end logits
+    start_logits = outputs['start_logits']
+    end_logits = outputs['end_logits']
+    # start_logits, end_logits = outputs[0], outputs[1]
 
     #Get the most likely start and end token positions
     start_idx = tf.argmax(start_logits, axis=1).numpy()[0]
     end_idx = tf.argmax(end_logits, axis=1).numpy()[0]
 
+    #Error handling
+    if start_idx > end_idx:
+        return "Unable to find a valid answer."
+
     #Convert token inx back to the text
     tokens = input_ids[0].numpy()[start_idx:end_idx+1]
-    answer = tokenizer.decode(tokens)
+    answer = tokenizer.decode(tokens,
+                              skip_special_tokens=True,
+                              clean_up_tokenization_spaces=True)
 
     return answer
