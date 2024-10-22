d714338090e8799f6559aa91536508a31eb8a252
AlecoPB <ajoaquinpg@gmail.com>
2024-10-22 15:23:13 +0200

    Update 0-load_env.py

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -6,6 +6,9 @@ import gymnasium as gym
 
 
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
+    """
+    Generate a FrozenLake map
+    """
     return gym.make('FrozenLake-v1',
                     desc=desc,
                     map_name=map_name,
