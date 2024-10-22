a839ab0e7928cb0a65f4ac83ac2ed73b49270201
AlecoPB <ajoaquinpg@gmail.com>
2024-10-22 15:21:39 +0200

    a

---
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This is some documentation
+"""
+import gymnasium as gym
+
+
+def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
+    return gym.make('FrozenLake-v1',
+                    desc=desc,
+                    map_name=map_name,
+                    is_slippery=is_slippery)
---
+++ reinforcement_learning/q_learning/README.md
@@ -0,0 +1,1 @@
+This README file is dedicated to q-learning
