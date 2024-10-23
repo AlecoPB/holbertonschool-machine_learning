ca5f286930468413f2bde317874bcf98f2a0df2e
AlecoPB <ajoaquinpg@gmail.com>
2024-10-23 12:03:32 +0200

    Update 1-q_init.py

--- reinforcement_learning/q_learning/1-q_init.py
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -10,6 +10,8 @@ def q_init(env):
     """
     Initializes a q table with zeros
     """
-    states, actions = env.observation_space.n, env.action_space.n
+    actions = env.action_space.n
+    states = env.observation_space.n 
     return np.zeros(states, actions)
+
 
