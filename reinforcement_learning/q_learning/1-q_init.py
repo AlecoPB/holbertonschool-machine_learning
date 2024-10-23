e1033b012ee3b0ffb4d82159f1321fbb67ef3c26
AlecoPB <ajoaquinpg@gmail.com>
2024-10-23 11:48:31 +0200

    Update 1-q_init.py

--- reinforcement_learning/q_learning/1-q_init.py
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -3,8 +3,13 @@
 This is some documentation
 """
 import gymnasium as gym
-load_frozen_lake =  '0-load_env'.load_frozen_lake
+import numpy as np
+# load_frozen_lake =  '0-load_env'.load_frozen_lake
 
 
 def q_init(env):
-    
+    """
+    Initializes a q table with zeros
+    """
+    h, w = env.shape()
+    return np.zeros(h, w)
