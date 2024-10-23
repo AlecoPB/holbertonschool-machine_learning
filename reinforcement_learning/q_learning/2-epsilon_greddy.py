22278ece80c4302aa272c6438c4f923bc1108ed3
AlecoPB <ajoaquinpg@gmail.com>
2024-10-23 14:49:19 +0200

    Create 2-epsilon_greddy.py

---
+++ reinforcement_learning/q_learning/2-epsilon_greddy.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+This is some documentation
+"""
+import numpy as np
+
+
+def epsilon_greedy(Q, state, epsilon):
+    """
+    Epsilon greedy implementation
+    """
+    # Random number to decide if explore or exploit
+    p = np.random.uniform
+
+    # Check if p is less than our epsilon hyperparamenter
+    if p < epsilon:
+        action = np.random.randint(0, Q.shape[0]) # Explore
+    else:
+        action = np.argmax(Q[state]) # Eploit
+
+    return action
