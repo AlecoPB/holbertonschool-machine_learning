ffe6caac504d05186f36cbcf27aeeb88da2c1537
AlecoPB <ajoaquinpg@gmail.com>
2024-10-23 14:57:35 +0200

    Update 2-epsilon_greedy.py

--- reinforcement_learning/q_learning/2-epsilon_greedy.py
+++ reinforcement_learning/q_learning/2-epsilon_greedy.py
@@ -10,11 +10,11 @@ def epsilon_greedy(Q, state, epsilon):
     Epsilon greedy implementation
     """
     # Random number to decide if explore or exploit
-    p = np.random.uniform(0, Q.shape[0])
+    p = np.random.uniform(0, 1)
 
     # Check if p is less than our epsilon hyperparamenter
     if p < epsilon:
-        action = np.random.randint(0, Q.shape[0]) # Explore
+        action = np.random.randint(0, Q.shape[1]) # Explore
     else:
         action = np.argmax(Q[state]) # Eploit
 
