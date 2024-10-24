524b9d3324a462900fd6562bafb36ffaf9480138
AlecoPB <ajoaquinpg@gmail.com>
2024-10-24 09:54:54 +0200

    Update 4-play.py

--- reinforcement_learning/q_learning/4-play.py
+++ reinforcement_learning/q_learning/4-play.py
@@ -3,21 +3,32 @@ import numpy as np
 
 
 def play(env, Q, max_steps=100):
-    state = env.reset()
+    """
+    Plays and renders a map
+    """
+    state, _ = env.reset()
     total_reward = 0
     rendered_outputs = []
 
     for _ in range(max_steps):
-        rendered_output = env.render
+
+        # Render and append
+        rendered_output = env.render()
         rendered_outputs.append(rendered_output)
 
+        # Decide and take action
         action = np.argmax(Q[state])  # Exploit
-        next_state, reward, done, info = env.step(action)
+        next_state, reward, done, info, _ = env.step(action)
 
+        # We assign reward each time, since it can only be 1
         total_reward = reward
 
         state = next_state
 
+        if done:
+            break
+
+    # We render one last time
     rendered_output = env.render()
     rendered_outputs.append(rendered_output)
 
