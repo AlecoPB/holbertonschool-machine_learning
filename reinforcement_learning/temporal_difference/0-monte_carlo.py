ac0a2c193bbe2b8bdba1e1f31a0886e8f48d71aa
AlecoPB <ajoaquinpg@gmail.com>
2024-11-07 10:50:59 +0100

    Update 0-monte_carlo.py

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -19,34 +19,33 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1, gamma=0
         Updated value function V.
     """
 
-    for _ in range(episodes):
-        state = env.reset()
-        episode_data = []
+    for episode in range(episodes):
+        state = env.reset()[0]
+        episode_data = []  # Store (state, reward) tuples
 
         # Generate an episode
-        for _ in range(max_steps):
+        for t in range(max_steps):
             action = policy(state)  # Get action from policy
-            next_state, reward, done, _, _ = env.step(action)
+            #Take action
+            next_state, reward, done, truncated, _= env.step(action)
 
             episode_data.append((state, reward))  # Record (state, reward) pair
+            state = next_state  # Update state
 
             if done:
                 break  # End episode if 'done' signal is received
 
-            state = next_state  # Update state
-
         # Process the episode to calculate returns and update V
         G = 0  # Initialize return (G_t) as 0
-        visited_states = set()  # Track visited states for first-visit MC
+        episode_data = np.array(episode_data, dtype=int)
 
         # Loop backwards through episode data
         for state, reward in reversed(episode_data):
-            G = reward + gamma * G  # Compute return G_t
+            # Compute episode's return
+            G = reward + gamma * G
 
             # First-visit MC: Update V on first occurrence only
-            if state not in visited_states:
-                visited_states.add(state)
-
+            if state not in episode_data[:episode, 0]:
                 # Update value function V
                 V[state] = V[state] + alpha * (G - V[state])
 
