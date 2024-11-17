8c05919564878140b3b2bac9d1ce33131fb4348c
AlecoPB <ajoaquinpg@gmail.com>
2024-11-17 22:22:00 +0100

    Update train.py

--- reinforcement_learning/policy_gradients/train.py
+++ reinforcement_learning/policy_gradients/train.py
@@ -1,70 +1,70 @@
 #!/usr/bin/env python3
 """
-This is some documenation
+Training loop for MC policy gradient
 """
 import numpy as np
 policy_gradient = __import__('policy_gradient').policy_gradient
 
 
-def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
-    """Full training
+def train(env, nb_episodes, alpha=0.000045, gamma=0.98, show_result=False):
+    """
+    Train the policy using Monte-Carlo policy gradient.
 
-    Args:
+    Parameters:
         env: initial environment
-        nb_episodes (int): number of episodes used for training
-        alpha (float, optional): learning rate. Defaults to 0.000045.
-        gamma (float, optional): discount factor. Defaults to 0.98.
+        nb_episodes: number of episodes used for training
+        alpha: the learning rate
+        gamma: the discount factor
+        show_result (boolean): if True, renders the environment every 1000
+            episodes. Defaults to False.
 
     Returns:
-        sum of all rewards during one episode loop
+        list: Score values (rewards obtained during each episode)
     """
+    # Initialize weights
+    weights = np.random.rand(
+        env.observation_space.shape[0],
+        env.action_space.n
+    )
     scores = []
 
-    # Determine dimensions for the weight matrix
-    state_size = env.observation_space.shape[0]
-    num_actions = env.action_space.n
-
-    # Generate a random weight matrix
-    weight = np.random.randn(state_size, num_actions)
-
     for episode in range(nb_episodes):
-        
+        # Reset the environment and get initial state
         state = env.reset()[0]
-        rewards, gradients = [], []
-        ep_rewards = []
-
-        # Initialize and reset G
-        G = 0
-
+        episode_gradients = []
+        episode_rewards = []
         done = False
 
+        if show_result and episode % 1000 == 0:
+            env.render()
+
         while not done:
-            # Decide action and gradient using policy_gradient
-            action, gradient = policy_gradient(state, weight)
+            # Get action and gradient based on current policy
+            action, grad = policy_gradient(state, weights)
 
-            # Take step and record state and rewards
-            new_state, reward, done, truncated, _ = env.step(action)
-            rewards.append(reward)
-            gradients.append(gradient)
+            # Take action
+            next_state, reward, terminated, truncated, _ = env.step(action)
 
-            # Update state
-            state = new_state
+            # Append reward and gradient to the episode history
+            episode_rewards.append(reward)
+            episode_gradients.append(grad)
 
-            done = done or truncated
+            state = next_state
+            done = terminated or truncated
 
-        # Compute total reward for the episode
-        scores.append(sum(rewards))
+        # Store the score for the episode
+        score = sum(episode_rewards)
+        scores.append(score)
 
-        # Print current episode and score
-        print(f'Episode: {episode} Score: {sum(rewards)}')        
+        # Print progress for every episode
+        print(f"Episode: {episode} Score: {score}")
 
         # Update weights using the gradients and cumulative discounted rewards
-        for i, gradient in enumerate(gradients):
-
+        for i, gradient in enumerate(episode_gradients):
             # Calculate cumulative discounted rewards
-            reward = sum([R * gamma ** R for R in rewards[i:]])
+            reward = sum([R * gamma ** R for R in episode_rewards[i:]])
 
             # Update the weights
-            weight += alpha * gradient * reward
+            weights += alpha * gradient * reward
 
     return scores
