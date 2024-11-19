94d6b463c611a23890fe9ef2705a577c0189a6fc
AlecoPB <ajoaquinpg@gmail.com>
2024-11-19 13:49:47 +0100

    Created directory

---
+++ pipeline/pandas/0-from_numpy.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Function to create a DataFrame from a np.array
+"""
+import numpy as np
+import pandas as pd
+
+
+def from_numpy(array):
+    """Creates a pd.DataFrame from a np.ndarray
+
+    Args:
+        array (np.ndarray): to transform into a DF
+    """
+    columns = [chr(i) for i in range(65, 91)]
+    return pd.DataFrame(array, columns=columns)
