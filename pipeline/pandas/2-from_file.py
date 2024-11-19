8ebb6129ab7dba896ed0d9631c4bfea3e9bfe3d6
AlecoPB <ajoaquinpg@gmail.com>
2024-11-19 14:23:19 +0100

    Create 2-from_file.py

---
+++ pipeline/pandas/2-from_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Function to create a DataFrame from a file
+"""
+import pandas as pd
+
+
+def from_numpy(filename, delimiter):
+    """Creates a pd.DataFrame from a np.ndarray
+
+    Args:
+        array (np.ndarray): to transform into a DF
+    """
+    
+    return pd.DataFrame(pd.read_csv(filename, delimiter=delimiter))
