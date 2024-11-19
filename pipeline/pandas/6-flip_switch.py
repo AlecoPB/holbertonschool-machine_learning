8c71531147ae2a8fef5f5d9e56e359e31a053907
AlecoPB <ajoaquinpg@gmail.com>
2024-11-19 15:43:40 +0100

    Update 6-flip_switch.py

--- pipeline/pandas/6-flip_switch.py
+++ pipeline/pandas/6-flip_switch.py
@@ -5,6 +5,11 @@ Modifies a DF obtained from a file
 
 
 def flip_switch(df):
+    """Sorts and flips
+
+    Args:
+        df (DataFrame)
+    """
     return df.sort_values(by=['Timestamp'],
                           ascending=False).T
     
