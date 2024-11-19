40999acff5acf252943526567b7bf6a8bf28abeb
AlecoPB <ajoaquinpg@gmail.com>
2024-11-19 14:18:55 +0100

    Update 1-from_dictionary.py

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -5,5 +5,5 @@ Function to create a DataFrame from a np.array
 import pandas as pd
 
 
-dict = {'first' : [0.0, 0.5, 1.0, 1.5], 'second' : ['one', 'two', 'three', 'four']}
+dict = {'First' : [0.0, 0.5, 1.0, 1.5], 'Second' : ['one', 'two', 'three', 'four']}
 df = pd.DataFrame(dict, index=['A', 'B', 'C', 'D'])
