dda4fa06a24a653a3cca629fc9fa177563815a33
AlecoPB <ajoaquinpg@gmail.com>
2024-11-19 14:55:28 +0100

    Create 3-rename.py

---
+++ pipeline/pandas/3-rename.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Modifies a DF obtained from a file
+"""
+import pandas as pd
+
+
+def rename(df):
+    """Modify DF
+
+    Args:
+        df (pd.DataFrame): DF containing Timestamp
+    """
+    new_df = pd.DataFrame(df)
+    new_df.rename(columns={'Timestamp': 'DateTime'}, inplace=True)
+    new_df['DateTime'] = pd.to_datetime(new_df['DateTime'])
+
+    return new_df[['DateTime', 'Close']]
