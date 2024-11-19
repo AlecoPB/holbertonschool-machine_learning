eadf76801cb3eda6d5657afde71901124b0e8f17
AlecoPB <ajoaquinpg@gmail.com>
2024-11-19 14:25:18 +0100

    Update 2-from_file.py

--- pipeline/pandas/2-from_file.py
+++ pipeline/pandas/2-from_file.py
@@ -5,7 +5,7 @@ Function to create a DataFrame from a file
 import pandas as pd
 
 
-def from_numpy(filename, delimiter):
+def from_file(filename, delimiter):
     """Creates a pd.DataFrame from a np.ndarray
 
     Args:
