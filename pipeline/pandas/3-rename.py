992002ea9ba855e95b9547d549a8aaf977e41864
AlecoPB <ajoaquinpg@gmail.com>
2024-11-19 14:57:22 +0100

    Update 3-rename.py

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -13,6 +13,6 @@ def rename(df):
     """
     new_df = pd.DataFrame(df)
     new_df.rename(columns={'Timestamp': 'DateTime'}, inplace=True)
-    new_df['DateTime'] = pd.to_datetime(new_df['DateTime'])
+    new_df['DateTime'] = pd.to_datetime(new_df['DateTime'], unit='s')
 
     return new_df[['DateTime', 'Close']]
