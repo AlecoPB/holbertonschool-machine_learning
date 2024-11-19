d360f7459ecd13dbae2bf27baf11eb308f4297ac
AlecoPB <ajoaquinpg@gmail.com>
2024-11-19 13:51:18 +0100

    Update 0-from_numpy.py

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -11,5 +11,5 @@ def from_numpy(array):
     Args:
         array (np.ndarray): to transform into a DF
     """
-    columns = [chr(i) for i in range(65, 91)]
+    columns = [chr(i) for i in range(65, 65+len(array))]
     return pd.DataFrame(array, columns=columns)
