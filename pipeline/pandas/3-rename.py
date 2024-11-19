249057bc67d3c4a59b59347eddb6c2b6d3c56c0c
AlecoPB <ajoaquinpg@gmail.com>
2024-11-19 14:59:58 +0100

    Update 3-rename.py

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -12,7 +12,7 @@ def rename(df):
         df (pd.DataFrame): DF containing Timestamp
     """
     new_df = pd.DataFrame(df)
-    new_df.rename(columns={'Timestamp': 'DateTime'}, inplace=True)
+    new_df.rename(columns={'Timestamp': 'Datetime'}, inplace=True)
     new_df['DateTime'] = pd.to_datetime(new_df['DateTime'], unit='s')
 
     return new_df[['DateTime', 'Close']]
