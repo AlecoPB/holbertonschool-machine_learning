0388b97885701f1c1318060b0fb3794b955db6ba
AlecoPB <ajoaquinpg@gmail.com>
2024-11-20 17:14:46 +0100

    Update 11-concat.py

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -15,7 +15,7 @@ def concat(df1, df2):
     df1.set_index('Timestamp', inplace=True)
     df2.set_index('Timestamp', inplace=True)
 
-    df2= df2.loc[:1417411920]
+    df2 = df2.loc[:1417411920]
 
     df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
 
