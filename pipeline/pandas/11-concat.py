3f129f825d34ff1a9a8353ec9abfec2887e06615
AlecoPB <ajoaquinpg@gmail.com>
2024-11-20 16:59:48 +0100

    Create 11-concat.py

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Indexes two DF's
+"""
+import pandas as pd
+index = __import__('10-index').index
+
+
+def concat(df1, df2):
+    """Concatenate DataFrames
+
+    Args:
+        df1, df2 (DataFrame)
+    """
+    df1.set_index('Timestamp', inplace=True)
+    df2.set_index('Timestamp', inplace=True)
+    df = pd.concat([df2['Timestamp'], df1])
+    return df
