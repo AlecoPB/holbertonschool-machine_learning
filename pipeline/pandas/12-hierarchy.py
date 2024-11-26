8f40a7b19afca9cceb22245b4a82e982acdaaf0d
AlecoPB <ajoaquinpg@gmail.com>
2024-11-26 13:35:43 +0100

    Update 12-hierarchy.py

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -20,18 +20,17 @@ def hierarchy(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
     df1.set_index('Timestamp', inplace=True)
     df2.set_index('Timestamp', inplace=True)
 
-    # Filter both DataFrames for the timestamp range 1417411980 to 1417417980
-    start, end = 1417411980, 1417417980
-    df1 = df1.loc[start:end]
-    df2 = df2.loc[start:end]
+    # Filter the DataFrames for the specified timestamp range
+    df1_filtered = df1.loc[1417411980:1417417980]
+    df2_filtered = df2.loc[1417411980:1417417980]
 
-    # Concatenate DataFrames with keys to distinguish sources
-    df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'], names=['Source'])
+    # Concatenate the filtered DataFrames with specified keys
+    df = pd.concat([df2_filtered, df1_filtered], keys=['bitstamp', 'coinbase'])
 
-    # Ensure the resulting DataFrame is sorted by Timestamp
-    df.sort_index(level='Timestamp', inplace=True)
-
-    # Reset MultiIndex to move 'Timestamp' to the left, leaving 'Source' as a column
-    df = df.reset_index(level='Source')
+    # Rearrange the MultiIndex to have 'Timestamp' as the first level
+    df.index = df.index.set_levels(df.index.levels[0], level=1)
+    
+    # Sort the DataFrame by the MultiIndex to ensure chronological order
+    df.sort_index(level=0, inplace=True)
 
     return df
