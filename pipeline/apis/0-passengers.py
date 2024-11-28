432d4400ec5edf4f1b776ea3cb68596142b685b9
AlecoPB <ajoaquinpg@gmail.com>
2024-11-28 15:59:27 +0100

    Update 0-passengers.py

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -19,9 +19,10 @@ def availableShips(passengerCount):
 
     for i in range(70):
         # Get current ship
-        current_ship = requests.get('https://swapi-api.hbtn.io/api/starships/' + str(i))
+        current_ship = requests.get('https://swapi-api.hbtn.io/api/starships/'
+                                    + str(i))
 
-        # Set the name and passenger number of the current ship        
+        # Set the name and passenger number of the current ship 
         c_passengers = current_ship.json().get('passengers')
         c_name = current_ship.json().get('name')
 
