aa2a9e8b4cb4acf1f91d95f119a6e3854e2e587d
AlecoPB <ajoaquinpg@gmail.com>
2024-12-04 16:24:21 +0100

    Update 3-first_launch.py

--- pipeline/apis/3-first_launch.py
+++ pipeline/apis/3-first_launch.py
@@ -3,7 +3,6 @@
 This is some documentation
 """
 import requests
-from datetime import datetime
 
 
 def fetch_first_launch():
@@ -28,35 +27,17 @@ def fetch_first_launch():
     launchpad_id = first_launch['launchpad']
 
     # Fetch rocket and launchpad details
-    response = requests.get(f"https://api.spacexdata.com/v4/rockets/{rocket_id}")
-    rocket_name = response.json().get("name")
+    rocket_response = requests.get(f"https://api.spacexdata.com/v4/rockets/{rocket_id}")
+    rocket_name = rocket_response.json().get("name")
 
-    response = requests.get(f"https://api.spacexdata.com/v4/launchpads/{launchpad_id}")
-    launchpad_details = response.json()
+    pad_response = requests.get(f"https://api.spacexdata.com/v4/launchpads/{launchpad_id}")
+    launchpad_details = pad_response.json()
 
     # Format and print the result
     launchpad_name = launchpad_details.get("name")
     launchpad_locality = launchpad_details.get("locality")
     print(f"{name} ({date_local}) {rocket_name} - {launchpad_name} ({launchpad_locality})")
 
-
-def fetch_rocket_name(rocket_id):
-    """
-    Fetch the name of the rocket
-    """
-    if not rocket_id:
-        return "Unknown"
-    response = requests.get(f"https://api.spacexdata.com/v4/rockets/{rocket_id}")
-    rocket_data = response.json()
-    return rocket_data.get("name")
-
-def fetch_launchpad_details(launchpad_id):
-    """
-    Fetch the launchpad details (duh)
-    """
-    response = requests.get(f"https://api.spacexdata.com/v4/launchpads/{launchpad_id}")
-    response.raise_for_status()
-    return response.json()
 
 if __name__ == "__main__":
     fetch_first_launch()
