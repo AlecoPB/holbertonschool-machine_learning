23e8ad11650174a55a33ddfc2ce21b2813d2a7b8
AlecoPB <ajoaquinpg@gmail.com>
2024-12-05 17:49:06 +0100

    Update 10-count_shows_by_genre.sql

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -1,4 +1,6 @@
 -- counts how many shows belong to each genre
-SELECT tv_show_genres.genre_id, COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM tv_show_genres
-GROUP BY tv_show_genres.genre_id;
+SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
