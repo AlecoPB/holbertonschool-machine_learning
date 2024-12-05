e49f01a1e6ce645ea4ca6212503d71013b4906af
AlecoPB <ajoaquinpg@gmail.com>
2024-12-05 15:51:51 +0100

    Update 5-average.sql

--- pipeline/databases/5-average.sql
+++ pipeline/databases/5-average.sql
@@ -1,5 +1,2 @@
 -- compute the average of all records
-ALTER TABLE second_table
-ADD COLUMN average FLOAT;
-UPDATE second_table
-SET average = AVG(score);
+SELECT AVG(score) AS average FROM second_table;
