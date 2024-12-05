f7538f83b515025a18f55e76552aebd07d3a1022
AlecoPB <ajoaquinpg@gmail.com>
2024-12-05 18:02:15 +0100

    Update 11-rating_shows.sql

--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -1,5 +1,5 @@
 -- sorts all shows by their rating
-SELECT tv_shows.title, SUM(rate) as rating
+SELECT tv_shows.title, SUM(rate) AS rating
 FROM tv_shows
 JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.title
