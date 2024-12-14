139f2021ee6d68d69cfa6079283a18b4f514a39c
AlecoPB <ajoaquinpg@gmail.com>
2024-12-14 22:53:44 +0100

    Update 34-log_stats.py

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python3
 """
-Provides stats about Nginx logs stored in MongoDB
+This is some documentation
 """
 from pymongo import MongoClient
 
-
 def log_stats(logs_collection):
     """
-    Provides some formatted stats about Nginx logs stored in MongoDB.
+    Connects to the MongoDB `logs` database, retrieves statistics
+    from the `nginx` logs_collection, and prints them in the specified format.
     """
-
+    # Total number of logs
     total_logs = logs_collection.count_documents({})
     print(f"{total_logs} logs")
 
+    # Methods statistics
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
     for method in methods:
         count = logs_collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    status_check_count = logs_collection.count_documents({
+    # Logs with method=GET and path=/status
+    get_check_count = logs_collection.count_documents({
         "method": "GET",
         "path": "/status"
     })
-    print(f"{status_check_count} status check")
-
+    print(f"{get_check_count} status check")
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
