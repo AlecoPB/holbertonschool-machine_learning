d78b2fc1d7acf0c8c68a3e25b998e6c1942f5a52
AlecoPB <ajoaquinpg@gmail.com>
2025-01-20 15:53:04 +0100

    Update 6-train.py

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -8,24 +8,53 @@ create_train_op = __import__('5-create_train_op').create_train_op
 forward_prop = __import__('2-forward_prop').forward_prop
 
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
-    x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
+    nx = X_train.shape[1]
+    classes = Y_train.shape[1]
+
+    # Use previous functions to prepare placeholders & tensors
+    x, y = create_placeholders(nx, classes)
     y_pred = forward_prop(x, layer_sizes, activations)
     loss = calculate_loss(y, y_pred)
     accuracy = calculate_accuracy(y, y_pred)
     train_op = create_train_op(loss, alpha)
-    
-    init = tf.global_variables_initializer()
+
+    # Add an op to initialize the variables.
+    init_op = tf.global_variables_initializer()
+
+    # Add ops to save and restore all the variables (saver object)
     saver = tf.train.Saver()
+
+    # Add tensors to graph's collection
+    tf.add_to_collection('x', x)
+    tf.add_to_collection('y', y)
+    tf.add_to_collection('y_pred', y_pred)
+    tf.add_to_collection('loss', loss)
+    tf.add_to_collection('accuracy', accuracy)
+    tf.add_to_collection('train_op', train_op)
+
     with tf.Session() as sess:
-        sess.run(init)
-        for i in range(iterations):
-            t_cost, t_acc, _ = sess.run([loss, accuracy, train_op], feed_dict={x: X_train, y: Y_train})
+        # Initialize variables
+        sess.run(init_op)
+
+        # Training loop
+        for i in range(iterations + 1):
+            train_loss, train_accuracy = sess.run(
+                [loss, accuracy], feed_dict={x: X_train, y: Y_train})
+            valid_loss, valid_accuracy = sess.run(
+                [loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
+
+            # Print metrics after every 100 iterations, the 0th & the last
             if i % 100 == 0 or i == iterations:
-                v_cost, v_acc = sess.run([loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
-                print("After {} iterations:".format(i))
-                print("\tTraining Cost: {}".format(t_cost))
-                print("\tTraining Accuracy: {}".format(t_acc))
-                print("\tValidation Cost: {}".format(v_cost))
-                print("\tValidation Accuracy: {}".format(v_acc))
-                
-    return saver.save(sess, save_path)
+                print(f"After {i} iterations:")
+                print(f"\tTraining Cost: {train_loss}")
+                print(f"\tTraining Accuracy: {train_accuracy}")
+                print(f"\tValidation Cost: {valid_loss}")
+                print(f"\tValidation Accuracy: {valid_accuracy}")
+
+            if i < iterations:
+                sess.run(train_op, feed_dict={x: X_train, y: Y_train})
+
+        # Save the model
+        save_path = saver.save(sess, save_path)
+
+    return save_path
