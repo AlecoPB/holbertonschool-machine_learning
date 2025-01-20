45c91ceefdd0fb90bf3761b1c8f7e6aa14f710ff
AlecoPB <ajoaquinpg@gmail.com>
2025-01-20 16:06:03 +0100

    Update 7-evaluate.py

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -14,13 +14,21 @@ def evaluate(X, Y, save_path):
 
     returns: the network's data
     """
-    # Load the model
-    model = tf.keras.models.load_model(save_path)
+    with tf.Session() as sess:
+        # Restore model from file
+        saver = tf.train.import_meta_graph(save_path + ".meta")
+        saver.restore(sess, save_path)
 
-    # Evaluate the model
-    loss, accuracy = model.evaluate(X, Y, verbose=0)
+        # Get placeholders and tensors from collection
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        loss = tf.get_collection("loss")[0]
 
-    # Get predictions
-    predictions = model.predict(X)
+        # Compute prediction, accuracy and loss
+        prediction = sess.run(y_pred, feed_dict={x: X, y: Y})
+        accuracy = sess.run(accuracy, feed_dict={x: X, y: Y})
+        loss = sess.run(loss, feed_dict={x: X, y: Y})
 
-    return predictions, accuracy, loss
+    return prediction, accuracy, loss
