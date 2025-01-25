c8dc93972a807ec1987e2e680a783f1195c83b16
AlecoPB <ajoaquinpg@gmail.com>
2025-01-25 22:50:08 +0100

    Update 6-dropout_create_layer.py

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -14,12 +14,14 @@ def dropout_create_layer(prev, n, activation, keep_prob, training=True):
     - n: The number of nodes in the new layer.
     - activation: The activation function to be applied in the new layer.
     - keep_prob: The probability that a neuron will be retained.
-    - training: A boolean indicating if the model is currently in training mode.
+    - training: A boolean indicating if the model is
+    currently in training mode.
 
     Returns: The output tensor of the new layer.
     """
     # Initialize weights using He et al. method
-    init_weights = tf.keras.initializers.VarianceScaling(scale=2.0, mode="fan_avg")
+    init_weights = tf.keras.initializers.VarianceScaling(scale=2.0,
+                                                         mode="fan_avg")
 
     # Create the dense layer
     layer = tf.keras.layers.Dense(
