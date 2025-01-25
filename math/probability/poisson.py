a6178ec299817b78459b3c6d0eb225f935e7a3bc
AlecoPB <ajoaquinpg@gmail.com>
2025-01-26 00:26:40 +0100

    Create poisson.py

---
+++ math/probability/poisson.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+This is some documentation
+"""
+
+
+class Poisson:
+    """Class that represents a Poisson distribution."""
+
+    def __init__(self, data=None, lambtha=1.):
+        """Initialize a Poisson distribution instance.
+
+        Args:
+            data (list, optional): Data used to estimate the distribution.
+            lambtha (float): Expected number of occurrences in a given time frame.
+
+        Raises:
+            TypeError: If data is not a list.
+            ValueError: If data contains fewer than two data points.
+            ValueError: If lambtha is not a positive value.
+        """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data) / len(data))
