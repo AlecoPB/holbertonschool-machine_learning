5675946cc8d5d550677951c816888d6b3dc0a4a4
AlecoPB <ajoaquinpg@gmail.com>
2025-01-26 20:01:46 +0100

    Update 0-markov_chain.py

--- unsupervised_learning/hmm/0-markov_chain.py
+++ unsupervised_learning/hmm/0-markov_chain.py
@@ -10,10 +10,16 @@ def markov_chain(P, s, t=1):
     Determines the probability of a markov chain
     being in a particular state
     """
-    try:
-        result = s
-        for _ in range(t):
-            result = np.matmul(result, P)
-        return result
-    except:
+    if not isinstance(P, np.ndarray) or not isinstance(s, np.ndarray):
         return None
+    if P.ndim != s.ndim:
+        return None
+    if s.shape[1] != P.shape[0] or s.shape[1] != P.shape[1]:
+        return None
+    if not isinstance(t, int) or t < 0:
+        return None
+    if not np.allclose(P.sum(axis=1), 1) or not np.isclose(s.sum(), 1):
+        return None
+
+    # k-th state matrix: S{k} = S{0}.P^k, where k is equal to t in this case
+    return np.dot(s, np.linalg.matrix_power(P, t))
