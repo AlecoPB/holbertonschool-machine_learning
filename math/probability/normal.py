ad926a57dbc971b56ec8deb24bf886ee37769a34
AlecoPB <ajoaquinpg@gmail.com>
2025-01-26 14:25:36 +0100

    Update normal.py

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -55,3 +55,14 @@ class Normal:
             float: The x-value corresponding to z.
         """
         return z * self.stddev + self.mean
+
+    def pdf(self, x):
+        """Calculate the PDF for a given x-value.
+
+        Args:
+            x (float): The x-value.
+
+        Returns:
+            float: The PDF value for x.
+        """
+        return (1 / (self.stddev * (2 * 3.1415926536) ** 0.5)) * (2.7182818285 ** (-0.5 * ((x - self.mean) / self.stddev) ** 2))
