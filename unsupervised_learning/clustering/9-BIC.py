572d9214d89a23f74d552833cb8e18fcecd3809f
AlecoPB <ajoaquinpg@gmail.com>
2025-01-26 18:50:13 +0100

    Update 9-BIC.py

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -6,9 +6,6 @@ import numpy as np
 expectation_maximization = __import__('8-EM').expectation_maximization
 
 
-import numpy as np
-expectation_maximization = __import__('8-EM').expectation_maximization
-
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     """
     Determines the optimal number of clusters for a Gaussian Mixture Model using the
