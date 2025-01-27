6bc6ec3d4a2e00fc0410824befc206e291d719ff
AlecoPB <ajoaquinpg@gmail.com>
2025-01-27 14:40:06 +0100

    Update 7-densenet121.py

--- supervised_learning/deep_cnns/7-densenet121.py
+++ supervised_learning/deep_cnns/7-densenet121.py
@@ -18,23 +18,27 @@ def densenet121(growth_rate=32, compression=1.0):
     # Input tensor (assuming the given shape of data)
     input_tensor = K.Input(shape=(224, 224, 3))
 
-    # First BN-ReLU-Conv, with double the growth rate for the initial filter count
+    # First BN-ReLU-Conv, with double the growth rate
+
     nb_filters = growth_rate * 2
     batch_norm = K.layers.BatchNormalization()(input_tensor)
     relu_activation = K.layers.Activation(activation="relu")(batch_norm)
-    conv_layer = K.layers.Conv2D(filters=nb_filters,
-                                  kernel_size=(7, 7),
-                                  strides=(2, 2),
-                                  padding="same",
-                                  kernel_initializer=initializer)(relu_activation)
+    conv_layer =\
+        K.layers.Conv2D(filters=nb_filters,
+                        kernel_size=(7, 7),
+                        strides=(2, 2),
+                        padding="same",
+                        kernel_initializer=initializer)(relu_activation)
 
-    max_pool_layer = K.layers.MaxPool2D(pool_size=(3, 3),
-                                         strides=(2, 2),
-                                         padding="same")(conv_layer)
+    max_pool_layer =\
+        K.layers.MaxPool2D(pool_size=(3, 3),
+                           strides=(2, 2),
+                           padding="same")(conv_layer)
 
     # Dense block 1, transition layer 1, and so forth until block 4
     # NOTE: nb_filters is updated (halved) by transition_layer
-    block1, nb_filters = dense_block(max_pool_layer, nb_filters, growth_rate, 6)
+    block1, nb_filters = dense_block(max_pool_layer, nb_filters,
+                                     growth_rate, 6)
     trans1, nb_filters = transition_layer(block1, nb_filters, compression)
 
     block2, nb_filters = dense_block(trans1, nb_filters, growth_rate, 12)
@@ -46,10 +50,13 @@ def densenet121(growth_rate=32, compression=1.0):
     block4, nb_filters = dense_block(trans3, nb_filters, growth_rate, 16)
 
     # Average Pooling (7x7 global)
-    avg_pool_layer = K.layers.AvgPool2D(pool_size=(7, 7), strides=(1, 1))(block4)
+    avg_pool_layer = K.layers.AvgPool2D(pool_size=(7, 7),
+                                        strides=(1, 1))(block4)
 
     # Fully Connected Layer, softmax
-    output_layer = K.layers.Dense(units=1000, activation='softmax',
-                                   kernel_initializer=initializer)(avg_pool_layer)
+    output_layer =\
+        K.layers.Dense(units=1000,
+                       activation='softmax',
+                       kernel_initializer=initializer)(avg_pool_layer)
 
     return K.Model(inputs=input_tensor, outputs=output_layer)
