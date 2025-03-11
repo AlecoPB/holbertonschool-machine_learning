36744128dcc435ce2b3c8b576399b25a019a5a1d
AlecoPB <ajoaquinpg@gmail.com>
2025-03-11 16:47:27 +0100

    Update 6-bars.py

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
+"""
+Bar graph
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def bars():
+    """
+    Source code to plot a stacked bar graph
+    """
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4,3))
+    fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
 
     names = ['Farrah', 'Fred', 'Felicia']
